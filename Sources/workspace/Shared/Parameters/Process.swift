91bad36cca7d96fe8d1e81aa4f8c6f0735123865
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-01-06 17:27:02 -0600

    Linux compatibility (Process).

--- Sources/workspace/Shared/Parameters/Process.swift
+++ Sources/workspace/Shared/Parameters/Process.swift
@@ -9,6 +9,8 @@
 // Licensed under the Apache License, Version 2.0
 // See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
 
+import Foundation
+
 #if os(Linux)
     typealias Process = Task
 #endif

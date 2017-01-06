92cc8318e383934a9a52e80a8dc1e636913b429d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-01-06 14:06:31 -0600

    Restore from back‐up.

--- Sources/workspace/Shared/Repository/Repository.swift
+++ Sources/workspace/Shared/Repository/Repository.swift
@@ -27,6 +27,7 @@ struct Repository {
         fileprivate var allFilesIncludingWorkspaceItself: [String]?
         fileprivate var allFiles: [String]?
         fileprivate var printableListOfAllFiles: String?
+        fileprivate var packageDescription: File?
     }
     private static var cache = Cache()
     
@@ -35,10 +36,11 @@ struct Repository {
     private static let fileManager = FileManager.default
     private static let repositoryPath = fileManager.currentDirectoryPath
     
-    // MARK: - Interface
+    // MARK: - Repository
     
     static func resetCache() {
         cache = Cache()
+        Configuration.resetCache()
     }
     
     static var allFilesIncludingWorkspaceItself: [String] {
@@ -86,5 +88,46 @@ struct Repository {
     
     static var isEmpty: Bool {
         return allFiles.isEmpty
+    }
+    
+    // MARK: - Files
+    
+    static func read(file path: String) throws -> File {
+        
+        var encoding = String.Encoding.utf8
+        return File(path: path, contents: try String(contentsOfFile: path, usedEncoding: &encoding))
+    }
+    
+    static var packageDescription: File {
+        return cachedResult(cache: &cache.packageDescription) {
+            () -> File in
+            
+            return require() { try read(file: "Package.swift") }
+        }
+    }
+    
+    // MARK: - Actions
+    
+    static func delete(_ path: String) throws {
+        
+        try fileManager.removeItem(atPath: path)
+        
+        resetCache()
+    }
+    
+    static func copy(_ origin: String, to destination: String) throws {
+        
+        force() { try delete(destination) }
+        
+        try fileManager.copyItem(atPath: origin, toPath: destination)
+        
+        resetCache()
+    }
+    
+    static func move(_ origin: String, to destination: String) throws {
+        try copy(origin, to: destination)
+        try delete(origin)
+        
+        resetCache()
     }
 }

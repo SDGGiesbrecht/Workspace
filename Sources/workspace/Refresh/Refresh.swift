e18c85414eb7f9bed4703cdab8a015016cb0212b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-01-06 23:22:51 -0600

    Hide output message for now.

--- Sources/workspace/Refresh/Refresh.swift
+++ Sources/workspace/Refresh/Refresh.swift
@@ -9,7 +9,7 @@
 // Licensed under the Apache License, Version 2.0
 // See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
 
-let instructionsAfterRefresh = "Open \(Configuration.projectName).xcodeproj to work on the project."
+let instructionsAfterRefresh = "" //"Open \(Configuration.projectName).xcodeproj to work on the project."
 
 func runRefresh(andExit shouldExit: Bool) {
     

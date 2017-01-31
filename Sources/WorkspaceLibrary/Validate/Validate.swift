068e20cc88581057b81ee3947698e330b708b546
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-01-30 21:17:38 -0600

    Re‐engage iOS and tvOS tests.

--- Sources/WorkspaceLibrary/Validate/Validate.swift
+++ Sources/WorkspaceLibrary/Validate/Validate.swift
@@ -9,6 +9,8 @@
 // Licensed under the Apache License, Version 2.0
 // See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
 
+import Foundation
+
 import SDGCaching
 
 import SDGLogic
@@ -40,46 +42,58 @@ func runValidate(andExit shouldExit: Bool) {
     // Running unit tests...
     // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••
     
-    func runUnitTests(enabledInConfiguration: Bool, buildOnly: Bool, operatingSystemName: String, script: [String]) {
+    func printTestHeader(buildOnly: Bool, operatingSystemName: String) {
         
-        if enabledInConfiguration {
-            
-            let verbPhrase = buildOnly ? "Verifying build for" : "Running unit tests on"
-            // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••
-            printHeader(["\(verbPhrase) \(operatingSystemName)..."])
-            // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••
-            
-            if bash(script).succeeded {
-                let phrase = buildOnly ? "Build succeeds for" : "Unit tests succeed on"
-                individualSuccess(message: "\(phrase) \(operatingSystemName).")
-            } else {
-                let phrase = buildOnly ? "Build fails for" : "Unit tests fail on"
-                individualFailure(message: "\(phrase) \(operatingSystemName). (See above for details.)")
-            }
+        let verbPhrase = buildOnly ? "Verifying build for" : "Running unit tests on"
+        // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••
+        printHeader(["\(verbPhrase) \(operatingSystemName)..."])
+        // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••
+    }
+    
+    func runUnitTests(buildOnly: Bool, operatingSystemName: String, script: [String]) {
+        
+        if bash(script).succeeded {
+            let phrase = buildOnly ? "Build succeeds for" : "Unit tests succeed on"
+            individualSuccess(message: "\(phrase) \(operatingSystemName).")
+        } else {
+            let phrase = buildOnly ? "Build fails for" : "Unit tests fail on"
+            individualFailure(message: "\(phrase) \(operatingSystemName). (See above for details.)")
         }
     }
     
-    func runUnitTestsInSwiftPackageManager(enabledInConfiguration: Bool, operatingSystemName: String) {
-        runUnitTests(enabledInConfiguration: enabledInConfiguration, buildOnly: false, operatingSystemName: operatingSystemName, script: ["swift", "test"])
+    func runUnitTestsInSwiftPackageManager(operatingSystemName: String) {
+        printTestHeader(buildOnly: false, operatingSystemName: operatingSystemName)
+        runUnitTests(buildOnly: false, operatingSystemName: operatingSystemName, script: ["swift", "test"])
     }
     
-    runUnitTestsInSwiftPackageManager(enabledInConfiguration: Environment.operatingSystem == .macOS ∧ Configuration.supportMacOS, operatingSystemName: "macOS")
-    runUnitTestsInSwiftPackageManager(enabledInConfiguration: Environment.operatingSystem == .linux, operatingSystemName: "Linux")
+    if Environment.operatingSystem == .macOS ∧ Configuration.supportMacOS {
+        
+        // macOS
+        
+        runUnitTestsInSwiftPackageManager(operatingSystemName: "macOS")
+    }
+    
+    
+    
+    if Environment.operatingSystem == .linux {
+        
+        // Linux
+        
+        runUnitTestsInSwiftPackageManager(operatingSystemName: "Linux")
+    }
     
     if Environment.operatingSystem == .macOS {
         
         var deviceList: [String: String]?
         
-        func runUnitTestsInXcode(enabledInConfiguration: Bool, buildOnly: Bool, operatingSystemName: String, deviceKey: String) {
+        func runUnitTestsInXcode(buildOnly: Bool, operatingSystemName: String, deviceKey: String) {
             
-            if enabledInConfiguration ∧ ¬buildOnly {
-                // Manually launch the simulator to avoid timeouts.
-                let _ = bash(["killall", "Simulator"])
-                let _ = bash(["open", "-b", "com.apple.iphonesimulator"])
-            }
+            printTestHeader(buildOnly: buildOnly, operatingSystemName: operatingSystemName)
             
             let devices = cachedResult(cache: &deviceList) {
                 () -> [String: String] in
+                
+                print(["Searching for simulator..."], in: nil, spaced: true)
                 
                 guard let deviceManifest = bash(["instruments", "-s", "devices"]).output else {
                     fatalError(message: ["Failed to get list of simulators."])
@@ -112,16 +126,56 @@ func runValidate(andExit shouldExit: Bool) {
                     ])
             }
             
-            return runUnitTests(enabledInConfiguration: enabledInConfiguration, buildOnly: buildOnly, operatingSystemName: operatingSystemName, script: [
+            if ¬buildOnly {
+                // Manually launch the simulator to avoid timeouts.
+                let _ = bash(["open", "-b", "com.apple.iphonesimulator", "--args", "-CurrentDeviceUDID", deviceID])
+                
+                var decasecondsToWait: Int?
+                switch operatingSystemName {
+                case "iOS":
+                    decasecondsToWait = 12
+                case "tvOS":
+                    decasecondsToWait = 6
+                default:
+                    break
+                }
+                
+                if let decaseconds = decasecondsToWait {
+                    print(["Giving the simulator time to boot..."])
+                    for decasecond in (1 ... decaseconds).reversed() {
+                        print(["\(decasecond)0 s..."])
+                        sleep(10)
+                    }
+                }
+            }
+            
+            return runUnitTests(buildOnly: buildOnly, operatingSystemName: operatingSystemName, script: [
                 "xcodebuild", (buildOnly ? "build" : "test"),
                 "-scheme", Configuration.projectName,
                 "-destination", "id=\(deviceID)"
                 ])
         }
         
-        //runUnitTestsInXcode(enabledInConfiguration: Configuration.supportIOS, buildOnly: false, operatingSystemName: "iOS", deviceKey: "iPhone 7")
-        runUnitTestsInXcode(enabledInConfiguration: Configuration.supportWatchOS, buildOnly: true, operatingSystemName: "watchOS", deviceKey: "Apple Watch Series 2 - 38mm")
-        //runUnitTestsInXcode(enabledInConfiguration: Configuration.supportTVOS, buildOnly: false, operatingSystemName: "tvOS", platformKey: "tvOS", deviceKey: "Apple TV 1080p")
+        if Configuration.supportIOS {
+            
+            // iOS
+            
+            runUnitTestsInXcode(buildOnly: false, operatingSystemName: "iOS", deviceKey: "iPhone 7")
+        }
+        
+        if Configuration.supportWatchOS {
+            
+            // watchOS
+            
+            runUnitTestsInXcode(buildOnly: true, operatingSystemName: "watchOS", deviceKey: "Apple Watch Series 2 - 38mm")
+        }
+        
+        if Configuration.supportTVOS {
+            
+            // tvOS
+            
+            runUnitTestsInXcode(buildOnly: false, operatingSystemName: "tvOS", deviceKey: "Apple TV 1080p")
+        }
     }
     
     // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••

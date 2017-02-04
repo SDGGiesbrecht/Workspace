34f534995009965353c343024d6091fae22f1d26
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-02-04 15:50:31 -0600

    Apply file headers.

--- Sources/workspace/main.swift
+++ Sources/workspace/main.swift
@@ -1,13 +1,15 @@
-// main.swift
-//
-// This source file is part of the Workspace open source project.
-//
-// Copyright ©2017 Jeremy David Giesbrecht and the Workspace contributors.
-//
-// Soli Deo gloria
-//
-// Licensed under the Apache License, Version 2.0
-// See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+/*
+ main.swift
+
+ This source file is part of the Workspace open source project.
+
+ Copyright ©2017 Jeremy David Giesbrecht and the Workspace contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 import WorkspaceLibrary
 
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,13 +1,15 @@
-// LinuxMain.swift
-//
-// This source file is part of the Workspace open source project.
-//
-// Copyright ©2016–2017 Jeremy David Giesbrecht and the Workspace contributors.
-//
-// Soli Deo gloria
-//
-// Licensed under the Apache License, Version 2.0
-// See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+/*
+ LinuxMain.swift
+
+ This source file is part of the Workspace open source project.
+
+ Copyright ©2016–2017 Jeremy David Giesbrecht and the Workspace contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 import XCTest
 @testable import WorkspaceTests

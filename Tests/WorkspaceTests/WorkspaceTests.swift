b942a161343db0301296c8a60c4d1f7a8ea6d3b6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-02-06 18:56:08 -0600

    Test executables.

--- Tests/WorkspaceTests/WorkspaceTests.swift
+++ Tests/WorkspaceTests/WorkspaceTests.swift
@@ -1,13 +1,13 @@
 /*
  WorkspaceTests.swift
-
+ 
  This source file is part of the Workspace open source project.
  https://github.com/SDGGiesbrecht/Workspace
-
+ 
  Copyright ©2016–2017 Jeremy David Giesbrecht and the Workspace contributors.
-
+ 
  Soli Deo gloria.
-
+ 
  Licensed under the Apache Licence, Version 2.0.
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
@@ -686,6 +686,22 @@ class WorkspaceTests: XCTestCase {
                     }
                 }
                 XCTAssert(exists, "Broken link: \(link.url)")
+            }
+        }
+    }
+    
+    func testExecutables() {
+        if ¬Environment.isInXcode ∧ ¬Configuration.nestedTest {
+            
+            let executables: [String] = [
+                "Refresh Workspace (macOS).command",
+                "Refresh Workspace (Linux).sh",
+                "Validate Changes (macOS).command",
+                "Validate Changes (Linux).sh",
+                ]
+            
+            for file in executables {
+                XCTAssert(try! File(at: RelativePath("Resources/Scripts/\(file)")).isExecutable, "Script is not longer executable: \(file)")
             }
         }
     }

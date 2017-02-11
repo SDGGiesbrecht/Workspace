df6ef4ddd3ae4745735db2c1a06669f64fd85074
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-02-10 19:31:15 -0600

    Real rule.

--- Sources/WorkspaceLibrary/Proofread/Rules/CompatibilityCharacters.swift
+++ Sources/WorkspaceLibrary/Proofread/Rules/CompatibilityCharacters.swift
@@ -18,10 +18,16 @@ struct CompatibilityCharacters: Rule {
     
     static func check(file: File, status: inout Bool) {
         
+        // ½
+        
         var location = file.contents.startIndex
-        while let range = file.contents.range(of: "fwahahaha", in: location ..< file.contents.endIndex) {
+        while let range = file.contents.range(of: CharacterSet.decomposables, in: location ..< file.contents.endIndex) {
             
-            errorNotice(status: &status, file: file, range: range, replacement: "sorry", message: "This shouldn’t be here.")
+            let character = file.contents[range]
+            let normalized = character.decomposedStringWithCompatibilityMapping
+            if character ≠ normalized {
+                errorNotice(status: &status, file: file, range: range, replacement: normalized, message: "Some programs may lose “\(character)” in normalization. Use “\(normalized)” instead (with markup if necessary).")
+            }
             
             location = range.upperBound
         }

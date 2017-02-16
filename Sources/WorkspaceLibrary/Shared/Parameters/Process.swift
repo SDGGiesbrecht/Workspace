9119a7b131810655c4b1922d372c87fb88e8ac7a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-02-16 16:26:37 -0600

    Improved miscellaneous minor details.

--- Sources/WorkspaceLibrary/Shared/Parameters/Process.swift
+++ Sources/WorkspaceLibrary/Shared/Parameters/Process.swift
@@ -17,9 +17,9 @@ import Foundation
 #if os(Linux)
     // [_Workaround: Rename Process on Linux. (Swift 3.0.2)_]
     typealias Process = Task
-    
+
     extension Process {
-        
+
         // [_Workaround: Rename isRunning on Linux. (Swift 3.0.2)_]
         var isRunning: Bool {
             return running

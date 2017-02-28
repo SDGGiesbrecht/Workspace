2cd8cc8578ff08a8a6ddb3d27a72677d2c23767c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-02-27 21:11:43 -0600

    Modified generics exception.

--- Sources/WorkspaceLibrary/Proofread/Rules/ColonSpacing.swift
+++ Sources/WorkspaceLibrary/Proofread/Rules/ColonSpacing.swift
@@ -39,7 +39,7 @@ struct ColonSpacing: Rule {
                     ∧ ¬line.contains(":nodoc:") {
 
                     let inGenerics = linePrefix.contains("where ")
-                        ∨ (linePrefix.contains("<") ∧ ¬linePrefix.contains("(")) /* Generics */
+                        ∨ (linePrefix.contains("<") ∧ ¬linePrefix.contains("(") ∧ lineSuffix.contains(">")) /* Generics */
                     if let preceding = file.contents.substring(to: range.lowerBound).characters.last {
 
                         if preceding == " " {

7140e1f5867b551c902f11a84a926bfa524c7073
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-03-16 19:39:05 -0600

    Try build-only settings.

--- Sources/WorkspaceLibrary/Validate/Unit Tests/UnitTests.swift
+++ Sources/WorkspaceLibrary/Validate/Unit Tests/UnitTests.swift
@@ -185,7 +185,7 @@ struct UnitTests {
                 // ••••••• ••••••• ••••••• ••••••• ••••••• ••••••• •••••••
 
                 // [_Warning: Should not be silent._]
-                let settingsScriptResult = bash(script + ["\u{2D}showBuildSettings"]/*, silent: true*/)
+                let settingsScriptResult = bash(generateScript(buildOnly: true) + ["\u{2D}showBuildSettings"]/*, silent: true*/)
                 guard settingsScriptResult.succeeded,
                     let settings = settingsScriptResult.output else {
                     fatalError(message: [

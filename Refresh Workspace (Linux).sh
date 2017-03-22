91f55b9d8a76064875df03e4a1308f9b548dfbd8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-03-22 16:46:25 -0600

    Added rule exceptions.

--- Refresh Workspace (Linux).sh
+++ Refresh Workspace (Linux).sh
@@ -3,7 +3,7 @@
 # Refresh Workspace (Linux).sh
 #
 # This source file is part of the Workspace open source project.
-# https://github.com/SDGGiesbrecht/Workspace
+# https://github.com/SDGGiesbrecht/Workspace#workspace
 #
 # Copyright ©2017 Jeremy David Giesbrecht and the Workspace project contributors.
 #

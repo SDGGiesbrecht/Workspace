01591e5b033fd7416db03c19dc9099ce962e66e3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-03-28 18:56:15 -0600

    Allowed Package.pins.

--- Tests/WorkspaceTests/WorkspaceTests.swift
+++ Tests/WorkspaceTests/WorkspaceTests.swift
@@ -624,6 +624,7 @@ class WorkspaceTests : XCTestCase {
             "Package.swift",
             "Sources",
             "Tests",
+            "Package.pins",
 
             // Workspace
             ".Workspace Configuration.txt",

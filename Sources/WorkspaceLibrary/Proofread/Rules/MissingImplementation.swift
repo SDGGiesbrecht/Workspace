d9ccc4a6c577723250d9e69056f1a9d2c2f1c4ea
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-06-05 17:21:01 -0600

    Added missing implementation rule.

---
+++ Sources/WorkspaceLibrary/Proofread/Rules/MissingImplementation.swift
@@ -0,0 +1,34 @@
+/*
+ MissingImplementation.swift
+
+ This source file is part of the Workspace open source project.
+ https://github.com/SDGGiesbrecht/Workspace#workspace
+
+ Copyright ©2017 Jeremy David Giesbrecht and the Workspace project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import Foundation
+
+import SDGCornerstone
+
+struct MissingImplementation : Rule {
+
+    static let name = "Missing Implementation"
+
+    static func check(file: File, status: inout Bool) {
+
+        var index = file.contents.startIndex
+        while let range = file.contents.range(of: "\u{6E}otImplementedYet", in: index ..< file.contents.endIndex) {
+            index = range.upperBound
+
+            if ¬file.contents.substring(to: range.lowerBound).hasSuffix("func ") {
+                errorNotice(status: &status, file: file, range: range, replacement: nil, message: "Missing implementation.")
+            }
+        }
+    }
+}

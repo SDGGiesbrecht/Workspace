3740f8d483070cabb0119298dd02a8f3ad3efefc
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-11-15 21:17:33 -0600

    Organized files.

---
+++ Sources/WorkspaceLibrary/External Tools/Third Party/SwiftPackage.swift
@@ -0,0 +1,13 @@
+/*
+ SwiftPackage.swift
+
+ This source file is part of the Workspace open source project.
+ https://github.com/SDGGiesbrecht/Workspace#workspace
+
+ Copyright ©2017 Jeremy David Giesbrecht and the Workspace project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
--- Sources/WorkspaceLibrary/Needs Refactoring/Validate/Documentation/WSDocumentation.swift
+++ Sources/WorkspaceLibrary/Needs Refactoring/Validate/Documentation/WSDocumentation.swift
@@ -1,5 +1,5 @@
 /*
- Documentation.swift
+ WSDocumentation.swift
 
  This source file is part of the Workspace open source project.
  https://github.com/SDGGiesbrecht/Workspace#workspace

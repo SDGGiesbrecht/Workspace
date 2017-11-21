b791850ce2f37c5905d6e577abe761c3109685d4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-11-20 18:12:12 -0600

    Restored job option.

--- Sources/WorkspaceLibrary/Interface/Validate/ValidateAll.swift
+++ Sources/WorkspaceLibrary/Interface/Validate/ValidateAll.swift
@@ -33,7 +33,9 @@ extension Workspace.Validate {
             }
         })
 
-        static let command = Command(name: name, description: description, directArguments: [], options: [], execution: { (arguments: DirectArguments, options: Options, output: inout Command.Output) throws in
+        static let command = Command(name: name, description: description, directArguments: [], options: [
+            ContinuousIntegration.Job.option
+            ], execution: { (arguments: DirectArguments, options: Options, output: inout Command.Output) throws in
             try runValidate(andExit: true, arguments: arguments, options: options, output: &output)
         })
     }

c5999a66b58c34cf1c17f4c2b2b2253f474a6a0d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-01-03 17:14:58 -0600

    ...

--- Tests/Mock Projects/After/SDG/Sources/Library/Resources.swift
+++ Tests/Mock Projects/After/SDG/Sources/Library/Resources.swift
@@ -5,6 +5,9 @@ import Foundation
 internal enum Resources {}
 
 extension Resources {
+    enum Namespace {
+        static let dataResource = Data(base64Encoded: "")!
+    }
     static let textResource = String(data: Data(base64Encoded: "SGVsbG8sIHdvcmxkIQ==")!, encoding: String.Encoding.utf8)!
 
 }

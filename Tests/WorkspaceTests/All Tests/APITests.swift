12ef3e338b76530cc63eb7d1663f18fbb787a2c5
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-02-07 14:34:01 -0600

    ...

--- Tests/WorkspaceTests/All Tests/APITests.swift
+++ Tests/WorkspaceTests/All Tests/APITests.swift
@@ -75,7 +75,7 @@ class APITests : TestCase {
                     var commands = try String(from: project.appendingPathComponent("$.txt")).components(separatedBy: "\n").filter({ ¬$0.isEmpty }).map { $0.components(separatedBy: " ").map({ StrictString($0) }) }
                     #if os(Linux)
                         commands = commands.filter { command in
-                            return ¬command.contains("documentation‐coverage")
+                            return ¬command.contains(where: { $0 ∈ Set<StrictString>(["documentation‐coverage", "macos‐swift‐package‐manager"])})
                         }
                     #endif
 

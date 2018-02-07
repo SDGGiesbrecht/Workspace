6ed8943997ce2de4449c261540152dcf6e927176
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-02-06 18:34:39 -0600

    ...

--- Tests/Mock Projects/Before/SDGLibrary/Sources/Library/SDG.swift
+++ Tests/Mock Projects/Before/SDGLibrary/Sources/Library/SDG.swift
@@ -1,10 +1,11 @@
 struct SDG {
     func text(_ bool: Bool) -> String {
-        if true {
+        if bool {
             return "Hello, World!"
         } else {
             return "Hello, World!"
-         /* The end of this line is not relevant to test coverage, but Xcode flags it. */ }
+            // The next line is not relevant to test coverage, but Xcode flags it.
+        }
     }
     
     func untestable() {

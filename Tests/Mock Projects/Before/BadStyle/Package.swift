39da4065bcd3d3227a962292f370d73f24c8af95
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-03-31 21:53:55 -0600

    Fixed dependency declaration in style test.

--- Tests/Mock Projects/Before/BadStyle/Package.swift
+++ Tests/Mock Projects/Before/BadStyle/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BadStyle",
-            dependencies: []),
+            dependencies: ["Dependency"]),
         .testTarget(
             name: "BadStyleTests",
             dependencies: ["BadStyle"])

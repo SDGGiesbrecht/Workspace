309bc622e35f5b210e2f7b37098b5545e4abbf4f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-04-11 14:17:18 -0600

    Resumed development.

--- Sources/WorkspaceLibrary/Program.swift
+++ Sources/WorkspaceLibrary/Program.swift
@@ -18,7 +18,7 @@ import SDGCommandLine
 
 // Do not forget to increment the version in “.Workspace Configuration.txt” as well.
 let latestStableWorkspaceVersion = Version(0, 6, 0)
-private let thisVersion: Version? = latestStableWorkspaceVersion // Set this to latestStableWorkspaceVersion for release commits, nil the rest of the time.
+private let thisVersion: Version? = nil // Set this to latestStableWorkspaceVersion for release commits, nil the rest of the time.
 
 let workspacePackageURL = URL(string: "https://github.com/SDGGiesbrecht/Workspace")!
 

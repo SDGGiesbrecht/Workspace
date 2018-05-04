fb21ee969747e5fc65527ea00f34b52841770804
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-04 16:57:28 -0600

    Updated pins and disambiguated module names.

--- Package.swift
+++ Package.swift
@@ -19,22 +19,30 @@ import PackageDescription
 let package = Package(
     name: "Workspace",
     products: [
-        .executable(name: "workspace", targets: ["workspace"]),
-        .executable(name: "arbeitsbereich", targets: ["workspace"])
+        .executable(name: "workspace", targets: ["WorkspaceTool"]),
+        .executable(name: "arbeitsbereich", targets: ["WorkspaceTool"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 8, 0))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCommandLine", .exact(Version(0, 2, 0)))
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 9, 3))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCommandLine", .exact(Version(0, 3, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 1, 1)))
     ],
     targets: [
-        .target(name: "workspace", dependencies: [.targetItem(name: "WorkspaceLibrary")]),
+        .target(name: "WorkspaceTool", dependencies: [.targetItem(name: "WorkspaceLibrary")]),
 
         .target(name: "WorkspaceLibrary", dependencies: [
-            .productItem(name: "SDGCornerstone", package: "SDGCornerstone"),
-            .productItem(name: "SDGCommandLine", package: "SDGCommandLine")
+            //.productItem(name: "SDGControlFlow", package: "SDGCornerstone"),
+            //.productItem(name: "SDGLogic", package: "SDGCornerstone"),
+            //.productItem(name: "SDGCollections", package: "SDGCornerstone"),
+            //.productItem(name: "SDGPersistence", package: "SDGCornerstone"),
+            //.productItem(name: "SDGLocalization", package: "SDGCornerstone"),
+            //.productItem(name: "SDGCommandLine", package: "SDGCommandLine"),
+            //.productItem(name: "SDGSwift", package: "SDGSwift"),
+            //.productItem(name: "SDGSwiftPackageManager", package: "SDGSwift"),
+            //.productItem(name: "SDGXcode", package: "SDGSwift")
             ]),
 
-        .testTarget(name: "WorkspaceTests", dependencies: [.targetItem(name: "WorkspaceLibrary")]),
+        .testTarget(name: "WorkspaceLibraryTests", dependencies: [.targetItem(name: "WorkspaceLibrary")]),
         .target(name: "test‐ios‐simulator", dependencies: [.targetItem(name: "WorkspaceLibrary")],
                 path: "Tests/test‐ios‐simulator"),
         .target(name: "test‐tvos‐simulator", dependencies: [.targetItem(name: "WorkspaceLibrary")],

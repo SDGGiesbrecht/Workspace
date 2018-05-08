bb70ead4431412f123e44b3a346ec731a354adef
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-07 22:06:29 -0600

    Refactored file iteration.

--- Sources/WorkspaceLibrary/Project/PackageRepository.swift
+++ Sources/WorkspaceLibrary/Project/PackageRepository.swift
@@ -167,52 +167,12 @@ extension PackageRepository {
     func allFiles() throws -> [URL] {
         return try cached(in: &cache.allFiles) {
             () -> [URL] in
-
-            var failureReason: Error? // Thrown after enumeration stops. (See below.)
-            guard let enumerator = FileManager.default.enumerator(at: location, includingPropertiesForKeys: [.isDirectoryKey, .isExecutableKey], options: [], errorHandler: { (_, error: Error) -> Bool in // [_Exempt from Test Coverage_] It is unknown what circumstances would actually cause an error.
-                failureReason = error
-                return false // Stop.
-            }) else { // [_Exempt from Test Coverage_] It is unknown what circumstances would actually result in a `nil` enumerator being returned.
-                throw Command.Error(description: UserFacing<StrictString, InterfaceLocalization>({ (localization) in // [_Exempt from Test Coverage_]
-                    switch localization {
-                    case .englishCanada: // [_Exempt from Test Coverage_]
-                        return "Cannot enumerate the project files."
-                    }
-                }))
+            let files = try FileManager.default.deepFileEnumeration(in: location).filter { url in
+                // Skip irrelevant operating system files.
+                return url.lastPathComponent ≠ ".DS_Store"
+                    ∧ ¬url.lastPathComponent.hasSuffix("~")
             }
-
-            var result: [URL] = []
-            for object in enumerator {
-                guard let url = object as? URL else { // [_Exempt from Test Coverage_] It is unknown why something other than a URL would be returned.
-                    throw Command.Error(description: UserFacing<StrictString, InterfaceLocalization>({ (localization) in // [_Exempt from Test Coverage_]
-                        switch localization {
-                        case .englishCanada: // [_Exempt from Test Coverage_]
-                            return StrictString("Unexpected \(type(of: object)) encountered while enumerating project files.")
-                        }
-                    }))
-                }
-
-                let isDirectory: Bool
-                #if os(Linux)
-                // [_Workaround: Linux has no implementation for resourcesValues(forKeys:) (Swift 4.0.2)_]
-                var objCBool: ObjCBool = false
-                isDirectory = FileManager.default.fileExists(atPath: url.path, isDirectory: &objCBool) ∧ objCBool.boolValue
-                #else
-                isDirectory = (try url.resourceValues(forKeys: [.isDirectoryKey])).isDirectory!
-                #endif
-
-                if ¬isDirectory, // Skip directories.
-                    url.lastPathComponent ≠ ".DS_Store", // Skip irrelevant operating system files.
-                    ¬url.lastPathComponent.hasSuffix("~") {
-
-                    result.append(url)
-                }
-            }
-
-            if let error = failureReason { // [_Exempt from Test Coverage_] It is unknown what circumstances would actually cause an error.
-                throw error
-            }
-            return result.sorted() // So that output order is consistent.
+            return files.sorted() // So that output order is consistent.
         }
     }
 

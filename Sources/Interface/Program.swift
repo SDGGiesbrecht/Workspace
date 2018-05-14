b9b1708a8b0230afc2019e85db062dc89ab07598
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-14 17:41:35 -0600

    Bumped version.

--- Sources/Interface/Program.swift
+++ Sources/Interface/Program.swift
@@ -15,8 +15,8 @@
 import GeneralImports
 
 // Do not forget to increment the version in “.Workspace Configuration.txt” as well.
-let latestStableWorkspaceVersion = Version(0, 6, 0)
-private let thisVersion: Version? = nil // Set this to latestStableWorkspaceVersion for release commits, nil the rest of the time.
+let latestStableWorkspaceVersion = Version(0, 7, 0)
+private let thisVersion: Version? = latestStableWorkspaceVersion // Set this to latestStableWorkspaceVersion for release commits, nil the rest of the time.
 
 let workspacePackageURL = URL(string: "https://github.com/SDGGiesbrecht/Workspace")!
 

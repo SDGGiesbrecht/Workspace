15b01984e43a0b5774d2e5905c18dce47d83099f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-14 22:58:50 -0600

    Resumed development.

--- Sources/Interface/Program.swift
+++ Sources/Interface/Program.swift
@@ -16,7 +16,7 @@ import GeneralImports
 
 // Do not forget to increment the version in “.Workspace Configuration.txt” as well.
 let latestStableWorkspaceVersion = Version(0, 7, 0)
-private let thisVersion: Version? = latestStableWorkspaceVersion // Set this to latestStableWorkspaceVersion for release commits, nil the rest of the time.
+private let thisVersion: Version? = nil // Set this to latestStableWorkspaceVersion for release commits, nil the rest of the time.
 
 let workspacePackageURL = URL(string: "https://github.com/SDGGiesbrecht/Workspace")!
 

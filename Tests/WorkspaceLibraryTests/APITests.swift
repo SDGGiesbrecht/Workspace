5fd81049225abb2865c2d9b55183e2422aeb7071
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-14 21:18:43 -0600

    ...

--- Tests/WorkspaceLibraryTests/APITests.swift
+++ Tests/WorkspaceLibraryTests/APITests.swift
@@ -151,12 +151,6 @@ class APITests : TestCase {
             ], configuration: WorkspaceConfiguration(), localizations: InterfaceLocalization.self, overwriteSpecificationInsteadOfFailing: false)
      }
 
-    func testNoFeatures() {/*
-        PackageRepository(mock: "NoFeatures").test(commands: [
-            ["refresh", "read‐me"]
-            ], localizations: InterfaceLocalization.self, overwriteSpecificationInsteadOfFailing: false)
-     */}
-
     func testNoLocalizations() {/*
         PackageRepository(mock: "NoLocalizations").test(commands: [
             ["refresh", "read‐me"]

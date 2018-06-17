ff13b9979c5e61e192f8da077ea22600b5c4fd6c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-16 18:23:38 -0600

    Converted file header documentation.

--- Sources/WorkspaceConfiguration/Configuration/FileHeaderConfiguration.swift
+++ Sources/WorkspaceConfiguration/Configuration/FileHeaderConfiguration.swift
@@ -13,6 +13,53 @@
  */
 
 /// Options related to file headers.
+///
+/// A file header is a commented section at the top of each file in a project. Typical uses for file headers include:
+///
+/// - Identifing which project the file belongs to.
+/// - Indicating copyright.
+/// - Providing licence reminders.
+///
+/// ## Precise Definition of a File Header
+///
+/// Because Workspace overwrites existing file headers, it is important to know how Workspace identifies them.
+///
+/// Workspace considers any comment that starts a file to be a file header, with the following constraints:
+///
+/// A file header may be a single block comment:
+///
+/// ```swift
+/// /*
+///  This is a header.
+///  This is more of the same header.
+///  */
+/// /* This is not part of the header. */
+/// ```
+///
+/// Alternatively, a file header may be a continous sequence of line comments:
+///
+/// ```swift
+/// // This is a header.
+/// // This is more of the same header.
+///
+/// // This is not part of the header.
+/// ```
+///
+/// Documentation comments are never headers.
+///
+/// ```swift
+/// /**
+///  This is not a header.
+///  */
+/// ```
+///
+/// In shell scripts, the shebang line precedes the header and is not part of it.
+///
+/// ```shell
+/// #!/bin/bash ← This is not part of the header.
+///
+/// # This is a header
+/// ```
 public struct FileHeaderConfiguration: Codable {
 
     /// Whether or not to manage the project file headers.
@@ -25,6 +72,12 @@ public struct FileHeaderConfiguration: Codable {
     /// By default, this is assembled from the other documentation and licence options.
     ///
     /// Workspace will replace the dynamic element `#dates` with the file’s copyright dates. (e.g. “©2016–2017”).
+    ///
+    /// ## Determination of the Dates
+    ///
+    /// Workspace uses any pre‐existing start date if it can detect one already in the file header. Workspace searches for `©`, `(C)`, or `(c)` followed by an optional space and four digits. If none is found, Workspace will use the current date as the start date.
+    ///
+    /// Workspace always uses the current date as the end date.
     public var copyrightNotice: Lazy<StrictString> = Lazy<StrictString>() { configuration in
         let project = StrictString(WorkspaceContext.current.manifest.packageName)
         if let author = configuration.documentation.primaryAuthor {

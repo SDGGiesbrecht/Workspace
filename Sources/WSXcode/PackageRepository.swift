073ae19f8f1c15a7ece34069e381dbbde74b14ac
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-26 22:51:30 -0600

    Fixed proofreading hook.

--- Sources/WSXcode/PackageRepository.swift
+++ Sources/WSXcode/PackageRepository.swift
@@ -23,6 +23,13 @@ extension PackageRepository {
 
     #if !os(Linux)
 
+    private static let proofreadTargetName = UserFacing<StrictString, InterfaceLocalization>({ localization in
+        switch localization {
+        case .englishCanada:
+            return "Proofread"
+        }
+    })
+
     private static let instructions = UserFacing<StrictString, InterfaceLocalization>({ localization in
         switch localization {
         case .englishCanada:
@@ -33,7 +40,21 @@ extension PackageRepository {
         }
     })
 
-    private static let scriptObjectName = "PROOFREAD"
+    private static let proofreadTargetIdentifier = "WORKSPACE_PROOFREAD_TARGET"
+    private static let proofreadScriptIdentifier = "WORKSPACE_PROOFREAD_SCRIPT"
+
+    private var aggregateTarget: String {
+        return  [
+            "        \(PackageRepository.proofreadTargetIdentifier) = {",
+            "            isa = PBXAggregateTarget;",
+            "            buildPhases = (",
+            "                \(PackageRepository.proofreadScriptIdentifier),",
+            "            );",
+            "            name = \u{22}\(PackageRepository.proofreadTargetName.resolved())\u{22};",
+            "            productName = \u{22}\(PackageRepository.proofreadTargetName.resolved())\u{22};",
+            "        };",
+            ].joinedAsLines()
+    }
 
     private func script() throws -> String {
         if try isWorkspaceProject() {
@@ -43,102 +64,44 @@ extension PackageRepository {
         }
     }
 
+    private func scriptObject() throws -> String {
+        return  [
+            "        \(PackageRepository.proofreadScriptIdentifier) = {",
+            "            isa = PBXShellScriptBuildPhase;",
+            "            shellPath = /bin/sh;",
+            "            shellScript = \u{22}\(try script())\u{22};",
+            "        };",
+            ].joinedAsLines()
+    }
+
     public func refreshXcodeProject(output: Command.Output) throws {
         try generateXcodeProject(reportProgress: { output.print($0) })
-        defer { resetFileCache(debugReason: "generate\u{2D}xcodeproj") }
+        resetFileCache(debugReason: "generate\u{2D}xcodeproj")
+        output.print("")
 
         if let projectBundle = try xcodeProject() {
-            let project = try TextFile(alreadyAt: projectBundle.appendingPathComponent("project.pbxproj"))
+            var project = try TextFile(alreadyAt: projectBundle.appendingPathComponent("project.pbxproj"))
 
+            let objectsLine = "objects = {"
+            if let range = project.contents.scalars.firstMatch(for: objectsLine.scalars)?.range {
+                project.contents.scalars.replaceSubrange(range, with: [
+                    objectsLine,
+                    aggregateTarget,
+                    try scriptObject(),
+                    ].joinedAsLines().scalars)
+            }
 
+            let targetsLine = "targets = ("
+            if let range = project.contents.scalars.firstMatch(for: targetsLine.scalars)?.range {
+                project.contents.scalars.replaceSubrange(range, with: [
+                    targetsLine,
+                    PackageRepository.proofreadTargetIdentifier + ",",
+                    ].joinedAsLines().scalars)
+            }
 
             try project.writeChanges(for: self, output: output)
         }
     }
 
     #endif
-
-    /*
-    #if !os(Linux)
-
-    struct DXcode {
-
-        static let scriptActionEntry = scriptObjectName + ","
-
-        static let skipProofreadingEnvironmentVariable = "SKIP_PROOFREADING"
-
-        static func enableProofreading(output: Command.Output) throws {
-
-            let allTargets = try Repository.packageRepository.targets().map { $0.name }
-            let primaryXcodeTarget = allTargets.first(where: { $0.scalars.first! ∈ CharacterSet.uppercaseLetters ∧ ¬$0.hasPrefix("Tests") })!
-
-            try modifyProject(condition: {
-                return ¬$0.contains("workspace proofread")
-
-            }, modification: { (file: inout TextFile) -> Void in
-
-                if let scriptInsertLocation = file.contents.firstMatch(for: "objects = {\n")?.range.upperBound {
-                    file.contents.replaceSubrange(scriptInsertLocation ..< scriptInsertLocation, with: [
-                        "\(scriptObjectName) = {",
-                        "    isa = PBXShellScriptBuildPhase;",
-                        "    shellPath = /bin/bash;",
-                        "    shellScript = \u{22}\(script)\u{22};",
-                        "};",
-                        "" // Final line break.
-                        ].joinedAsLines())
-
-                    var searchRange = file.contents.startIndex ..< file.contents.endIndex
-                    var discoveredPhaseInsertLocation: String.Index?
-                    while let possiblePhaseInsertLocation = file.contents.scalars.firstMatch(for: "buildPhases = (\n".scalars, in: searchRange.sameRange(in: file.contents.scalars))?.range.upperBound.cluster(in: file.contents.clusters) {
-                        searchRange = possiblePhaseInsertLocation ..< file.contents.endIndex
-
-                        if let name = file.contents.firstNestingLevel(startingWith: "name = \u{22}", endingWith: "\u{22};", in: searchRange).flatMap({ String($0.contents.contents) }) {
-                            if name == primaryXcodeTarget {
-                                discoveredPhaseInsertLocation = possiblePhaseInsertLocation
-                                break
-                            }
-                        }
-                    }
-
-                    guard let phaseInsertLocation = discoveredPhaseInsertLocation else {
-
-                        fatalError(message: [
-                            "Failed to find a target with the following name:",
-                            primaryXcodeTarget
-                            ])
-                    }
-
-                    file.contents.replaceSubrange(phaseInsertLocation ..< phaseInsertLocation, with: [
-                        scriptActionEntry,
-                        "" // Final line break.
-                        ].joinedAsLines())
-                }
-            }, output: output)
-        }
-
-        static let disabledScriptActionEntry = "/* " + scriptObjectName + " */"
-
-        static func temporarilyDisableProofreading(output: Command.Output) throws {
-
-            try modifyProject(condition: { (_) -> Bool in
-                return true
-            }, modification: { (file: inout TextFile) -> Void in
-
-                file.contents = file.contents.replacingOccurrences(of: scriptActionEntry, with: disabledScriptActionEntry)
-            }, output: output)
-        }
-
-        static func reEnableProofreading(output: Command.Output) throws {
-
-            try modifyProject(condition: { (_) -> Bool in
-                return true
-            }, modification: { (file: inout TextFile) -> Void in
-
-                file.contents = file.contents.replacingOccurrences(of: disabledScriptActionEntry, with: scriptActionEntry)
-            }, output: output)
-        }
-    }
-
-    #endif */
-
 }

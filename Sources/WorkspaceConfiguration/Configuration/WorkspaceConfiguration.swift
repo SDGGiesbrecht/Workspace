0875a2778cd53a9c00fb1f5025c1f356c9c4b881
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-28 17:27:01 -0600

    Fixed coding.

--- Sources/WorkspaceConfiguration/Configuration/WorkspaceConfiguration.swift
+++ Sources/WorkspaceConfiguration/Configuration/WorkspaceConfiguration.swift
@@ -233,7 +233,7 @@ public final class WorkspaceConfiguration : Configuration {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         provideWorkflowScripts = try container.decode(Bool.self, forKey: .provideWorkflowScripts)
         supportedOperatingSystems = try container.decode(Set<OperatingSystem>.self, forKey: .supportedOperatingSystems)
-        git = try container.decode(GitConfiguration.self, forKey: .git)
+        git = try container.decodeIfPresent(GitConfiguration.self, forKey: .git) ?? GitConfiguration()
         licence = try container.decode(LicenceConfiguration.self, forKey: .licence)
         fileHeaders = try container.decode(FileHeaderConfiguration.self, forKey: .fileHeaders)
         gitHub = try container.decode(GitHubConfiguration.self, forKey: .gitHub)

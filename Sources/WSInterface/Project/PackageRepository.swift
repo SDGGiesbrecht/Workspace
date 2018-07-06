ad592ec31d2d8aaae08a4f0d3605d0dc272c277f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-05 21:55:07 -0600

    Simplified directory names.

--- Sources/WSInterface/Project/PackageRepository.swift
+++ Sources/WSInterface/Project/PackageRepository.swift
@@ -117,8 +117,7 @@ extension PackageRepository {
     // MARK: - Related Projects
 
     public static func relatedPackage(_ package: SDGSwift.Package, output: Command.Output) throws -> PackageRepository {
-        var directoryName = StrictString(package.url.absoluteString)
-        directoryName.scalars.replaceMatches(for: ConditionalPattern({ $0 ∉ CharacterSet.alphanumerics }), with: "_".scalars)
+        var directoryName = StrictString(package.url.lastPathComponent)
         let cache = FileManager.default.url(in: .cache, at: "Related Projects/\(directoryName)")
 
         let commit = try package.latestCommitIdentifier()

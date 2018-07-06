18cae95ea24a56dba65c9aaeac12272ad7376176
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-05 22:32:11 -0600

    Refactored validation.

--- Sources/WSInterface/Interface/Validate/ValidateAll.swift
+++ Sources/WSInterface/Interface/Validate/ValidateAll.swift
@@ -12,7 +12,12 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
+import SDGCollections
 import WSGeneralImports
+
+import WorkspaceProjectConfiguration
+import WSValidation
 import WSContinuousIntegration
 
 extension Workspace.Validate {
@@ -37,13 +42,130 @@ extension Workspace.Validate {
             ContinuousIntegrationJob.option
             ], execution: { (arguments: DirectArguments, options: Options, output: Command.Output) throws in // [_Exempt from Test Coverage_] [_Workaround: Not testable yet._]
 
+                var validationStatus = ValidationStatus()
+
                 if options.job == .deployment {
                     try TravisCI.keepAlive {
-                        try runValidate(andExit: true, arguments: arguments, options: options, output: output)
+                        try executeAsStep(validationStatus: &validationStatus, arguments: arguments, options: options, output: output)
                     }
                 } else {
-                    try runValidate(andExit: true, arguments: arguments, options: options, output: output)
+                    try executeAsStep(validationStatus: &validationStatus, arguments: arguments, options: options, output: output)
                 }
         })
+
+        static func executeAsStep(validationStatus: inout ValidationStatus, arguments: DirectArguments, options: Options, output: Command.Output) throws {
+
+            if ¬ProcessInfo.isInContinuousIntegration {
+                try Workspace.Refresh.All.executeAsStep(withArguments: arguments, options: options, output: output)
+            }
+
+            let projectName = StrictString(try options.project.projectName())
+            output.print(UserFacing<StrictString, InterfaceLocalization>({ localization in
+                switch localization {
+                case .englishCanada:
+                    return "Validating “" + projectName + "”..."
+                }
+            }).resolved().formattedAsSectionHeader())
+
+            // Proofread
+            if options.job == .miscellaneous ∨ options.job == nil {
+                try Workspace.Proofread.executeAsStep(normalizingFirst: false, options: options, validationStatus: &validationStatus, output: output)
+            }
+
+            // Build
+            if try options.project.configuration(output: output).testing.prohibitCompilerWarnings {
+                try Workspace.Validate.Build.executeAsStep(options: options, validationStatus: &validationStatus, output: output)
+            }
+
+            // Test
+            #if os(Linux)
+            // Coverage irrelevant.
+            try Workspace.Test.executeAsStep(options: options, validationStatus: &validationStatus, output: output)
+            #else
+            if try options.project.configuration(output: output).testing.enforceCoverage {
+                if let job = options.job,
+                    job ∉ Tests.coverageJobs {
+                    // Coverage impossible to check.
+                    try Workspace.Test.executeAsStep(options: options, validationStatus: &validationStatus, output: output)
+                } else {
+                    // Check coverage.
+                    try Workspace.Validate.TestCoverage.executeAsStep(options: options, validationStatus: &validationStatus, output: output)
+                }
+            } else {
+                // Coverage irrelevant.
+                try Workspace.Test.executeAsStep(options: options, validationStatus: &validationStatus, output: output)
+            }
+            #endif
+
+            // Document
+            #if !os(Linux)
+            if options.job.includes(job: .documentation) {
+                if try options.project.configuration(output: output).documentation.api.enforceCoverage {
+                    try Workspace.Validate.DocumentationCoverage.executeAsStepDocumentingFirst(options: options, validationStatus: &validationStatus, output: output)
+                } else if try options.project.configuration(output: output).documentation.api.generate
+                    ∧ (try options.project.configuration(output: output).documentation.api.encryptedTravisCIDeploymentKey == nil) {
+                    try Workspace.Document.executeAsStep(outputDirectory: options.project.defaultDocumentationDirectory, options: options, validationStatus: &validationStatus, output: output)
+                }
+            }
+
+            if try options.job.includes(job: .deployment)
+                ∧ (try options.project.configuration(output: output).documentation.api.generate) {
+                try Workspace.Document.executeAsStep(outputDirectory: options.project.defaultDocumentationDirectory, options: options, validationStatus: &validationStatus, output: output)
+            }
+            #endif
+
+            // State
+            if ProcessInfo.isInContinuousIntegration ∧ ProcessInfo.isPullRequest {
+
+                let state = validationStatus.newSection()
+
+                output.print(UserFacing<StrictString, InterfaceLocalization>({ localization in
+                    switch localization {
+                    case .englishCanada:
+                        return "Validating project state..." + state.anchor
+                    }
+                }).resolved().formattedAsSectionHeader())
+
+                let difference = try options.project.uncommittedChanges(excluding: ["*.dsidx"])
+                if ¬difference.isEmpty {
+                    output.print(difference.separated())
+
+                    validationStatus.failStep(message: UserFacing({ localization in
+                        switch localization {
+                        case .englishCanada:
+                            return "The project is out of date. Please validate before committing." + state.crossReference.resolved(for: localization)
+                        }
+                    }))
+                } else {
+                    validationStatus.passStep(message: UserFacing({ localization in
+                        switch localization {
+                        case .englishCanada:
+                            return "The project is up to date."
+                        }
+                    }))
+                }
+            }
+
+            output.print("Summary".formattedAsSectionHeader())
+
+            // Workspace
+            if let update = try Workspace.CheckForUpdates.checkForUpdates(output: output) {
+                output.print(UserFacing<StrictString, InterfaceLocalization>({ localization in
+                    switch localization {
+                    case .englishCanada:
+                        let url = URL(string: "#installation", relativeTo: Metadata.packageURL)!
+                        return [
+                            StrictString("This validation used Workspace \(Metadata.latestStableVersion.string()), which is no longer up to date."),
+                            "To update the version used by this project, run:",
+                            StrictString("$ workspace refresh scripts •use‐version \(update.string)"),
+                            "(This requires a full installation. See the following link.)",
+                            StrictString("\(url.absoluteString.in(Underline.underlined))")
+                            ].joinedAsLines()
+                    }
+                }).resolved().formattedAsWarning().separated())
+            }
+
+            try validationStatus.reportOutcome(project: options.project, output: output)
+        }
     }
 }

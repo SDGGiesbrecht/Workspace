7e3840d1ae056dedb2b787dde8d56610b94d6be7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-08-23 18:31:42 -0600

    Fixed missing documentation detection.

--- Sources/WSDocumentation/SymbolPage.swift
+++ Sources/WSDocumentation/SymbolPage.swift
@@ -55,43 +55,49 @@ internal class SymbolPage : Page {
             symbolType = ""
         }
 
+        func reportMissingDocumentation() {
+            let description = UserFacing<StrictString, InterfaceLocalization>({ localization in
+                switch localization {
+                case .englishCanada:
+                    return "A symbol has no description:"
+                }
+            })
+            let symbolName: StrictString
+            switch symbol {
+            case is PackageAPI, is ModuleAPI:
+                symbolName = StrictString(symbol.name)
+            default:
+                symbolName = navigationPath.dropFirst().map({ StrictString($0.name) }).joined(separator: ".")
+            }
+            var hint: UserFacing<StrictString, InterfaceLocalization>?
+            if symbol is PackageAPI {
+                hint = UserFacing<StrictString, InterfaceLocalization>({ localization in
+                    switch localization {
+                    case .englishCanada:
+                        return "(The package can be documented in the package manifest the same way as other symbols. Workspace will look for documentation on the line above “Package(name: \u{22}" + StrictString(symbol.name) + "\u{22}”.)"
+                    }
+                })
+            }
+            status.report(warning: UserFacing({ localization in
+                var result: [StrictString] = [
+                    description.resolved(for: localization),
+                    symbolName
+                ]
+                if let theHint = hint {
+                    result.append(theHint.resolved(for: localization))
+                }
+                return result.joined(separator: "\n")
+            }))
+        }
+
         if let documentation = symbol.documentation {
             if let description = documentation.descriptionSection {
                 content.append(contentsOf: HTMLElement("div", attributes: ["class": "description"], contents: StrictString(description.renderedHTML()), inline: false).source)
             } else {
-                let description = UserFacing<StrictString, InterfaceLocalization>({ localization in
-                    switch localization {
-                    case .englishCanada:
-                        return "A symbol has no description:"
-                    }
-                })
-                let symbolName: StrictString
-                switch symbol {
-                case is PackageAPI, is ModuleAPI:
-                    symbolName = StrictString(symbol.name)
-                default:
-                    symbolName = navigationPath.dropFirst().map({ StrictString($0.name) }).joined(separator: ".")
-                }
-                var hint: UserFacing<StrictString, InterfaceLocalization>?
-                if symbol is PackageAPI {
-                    hint = UserFacing<StrictString, InterfaceLocalization>({ localization in
-                        switch localization {
-                        case .englishCanada:
-                            return "(The package can be documented in the package manifest the same way as other symbols. Workspace will look for documentation on the line above “Package(name: \u{22}" + StrictString(symbol.name) + "\u{22}”.)"
-                        }
-                    })
-                }
-                status.report(warning: UserFacing({ localization in
-                    var result: [StrictString] = [
-                        description.resolved(for: localization),
-                        symbolName
-                    ]
-                    if let theHint = hint {
-                        result.append(theHint.resolved(for: localization))
-                    }
-                    return result.joined(separator: "\n")
-                }))
+                reportMissingDocumentation()
             }
+        } else {
+            reportMissingDocumentation()
         }
 
         super.init(localization: localization, pathToSiteRoot: pathToSiteRoot, navigationPath: navigationPathLinks.joined(separator: "\n"), symbolType: symbolType, title: StrictString(symbol.name), content: content)

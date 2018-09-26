227f4041ca49a5ad4053520c06cb78eaf1369451
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-26 18:38:00 +0300

    Updated SDGSwift.

--- Package.swift
+++ Package.swift
@@ -136,7 +136,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 11, 0))),
         .package(url: "https://github.com/SDGGiesbrecht/SDGCommandLine", .exact(Version(0, 4, 0))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 3, 1))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 3, 2))),
         // #workaround(This should not be necessary.)
         .package(url: "https://github.com/apple/swift\u{2D}package\u{2D}manager", .exact(Version(0, 3, 0)))
     ],

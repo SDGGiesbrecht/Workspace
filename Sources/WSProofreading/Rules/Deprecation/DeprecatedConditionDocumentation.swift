4975171d9f746127729cc2381155b8f3bd50c99a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-10-05 18:35:40 +0300

    Refreshed.

--- Sources/WSProofreading/Rules/Deprecation/DeprecatedConditionDocumentation.swift
+++ Sources/WSProofreading/Rules/Deprecation/DeprecatedConditionDocumentation.swift
@@ -1,5 +1,5 @@
 /*
- DeprecatedConfiguration.swift
+ DeprecatedConditionDocumentation.swift
 
  This source file is part of the Workspace open source project.
  https://github.com/SDGGiesbrecht/Workspace#workspace

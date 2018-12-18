4d8ae54f15fa2a61c22c716d5432480a719e4620
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-12-17 21:23:10 -0600

    Loaded syntax.

--- Sources/WSProofreading/Rule.swift
+++ Sources/WSProofreading/Rule.swift
@@ -15,12 +15,14 @@
 import SDGLogic
 import WSGeneralImports
 
+import SDGSwiftSource
+
 import WSProject
 
 internal protocol Rule {
     static var name: UserFacing<StrictString, InterfaceLocalization> { get }
     static var noticeOnly: Bool { get }
-    static func check(file: TextFile, in project: PackageRepository, status: ProofreadingStatus, output: Command.Output) throws
+    static func check(file: TextFile, syntax: SourceFileSyntax?, in project: PackageRepository, status: ProofreadingStatus, output: Command.Output) throws
 }
 
 extension Rule {

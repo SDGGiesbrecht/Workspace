fcd003f48d3c19064e136f43b4e08d526f0dd25f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-12-19 14:47:19 -0600

    Fixed coverage check.

--- Sources/WSDocumentation/SymbolPage.swift
+++ Sources/WSDocumentation/SymbolPage.swift
@@ -131,7 +131,7 @@ internal class SymbolPage : Page {
         }
 
         if case .variable(let variable) = symbol,
-            variable.declaration.bindings.first?.typeAnnotation == nil {
+            variable.declaration.bindings.first?.typeAnnotation ≠ nil {
             status.reportMissingVariableType(variable, navigationPath: navigationPath)
         }
 

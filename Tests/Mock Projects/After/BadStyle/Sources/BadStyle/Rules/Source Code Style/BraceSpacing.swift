6a1f44d124a8c7365b17e2d553f70c51de5fa5a8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-01-16 20:42:24 -0600

    Added test.

--- Tests/Mock Projects/After/BadStyle/Sources/BadStyle/Rules/Source Code Style/BraceSpacing.swift
+++ Tests/Mock Projects/After/BadStyle/Sources/BadStyle/Rules/Source Code Style/BraceSpacing.swift
@@ -3,3 +3,7 @@
 let x = [].map {$0} // These braces should trigger; they should be spaced.
 func y() {} // These braces are fine; they are empty.
 func z() {/* These braces should trigger; they should be spaced. */}
+
+func þ() { // This brace should not trigger; it’s not empty.
+    // ...
+} // This brace should not trigger; it’s not empty.

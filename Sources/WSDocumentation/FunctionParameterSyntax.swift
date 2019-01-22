7f3cf3015e1a04591e45636e97bb3a294a46463c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-01-21 18:59:01 -0600

    Fixed parameter detection.

--- Sources/WSDocumentation/FunctionParameterSyntax.swift
+++ Sources/WSDocumentation/FunctionParameterSyntax.swift
@@ -1,9 +1,33 @@
+/*
+ FunctionParameterSyntax.swift
+
+ This source file is part of the Workspace open source project.
+ https://github.com/SDGGiesbrecht/Workspace#workspace
+
+ Copyright ©2019 Jeremy David Giesbrecht and the Workspace project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import SDGLogic
+import WSGeneralImports
 
 import SDGSwiftSource
 
 extension FunctionParameterSyntax {
 
     internal var parameterName: String {
-        return secondName?.text ?? firstName?.text ?? "" // @exempt(from: tests) One of the two should exist.
+        if let result = secondName?.text,
+            ¬result.isEmpty {
+            return result
+        } else if let result = firstName?.text,
+            ¬result.isEmpty {
+            return result
+        } else { // @exempt(from: tests) One of the two should exist.
+            return ""
+        }
     }
 }

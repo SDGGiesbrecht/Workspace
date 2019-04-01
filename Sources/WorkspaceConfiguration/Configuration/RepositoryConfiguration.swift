f457370b1b3c318dad6adbd6c1fb191e39dc013a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-03-31 20:51:36 -0600

    Handled new file types.

--- Sources/WorkspaceConfiguration/Configuration/RepositoryConfiguration.swift
+++ Sources/WorkspaceConfiguration/Configuration/RepositoryConfiguration.swift
@@ -21,6 +21,7 @@ public struct RepositoryConfiguration : Codable {
     ///
     /// Workspace automatically skips files it does not understand, but it prints a warning first. These warnings can be silenced by adding the file type to this list. For standard file types related to Swift projects, please [request that support be added](https://github.com/SDGGiesbrecht/Workspace/issues).
     public var ignoredFileTypes: Set<String> = [
+        "cfg",
         "dsidx",
         "DS_Store",
         "entitlements",

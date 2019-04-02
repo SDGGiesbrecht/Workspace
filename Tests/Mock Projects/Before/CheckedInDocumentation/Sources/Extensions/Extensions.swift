e88ae5d48cc38491a4355c42009a36683b7787a0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-02 14:04:19 -0600

    Added test.

--- Tests/Mock Projects/Before/CheckedInDocumentation/Sources/Extensions/Extensions.swift
+++ Tests/Mock Projects/Before/CheckedInDocumentation/Sources/Extensions/Extensions.swift
@@ -2,3 +2,13 @@ extension Structure {
     /// An extension method in another module.
     public func extensionMethodInAnotherModule() {}
 }
+
+extension Protocol {
+    /// An extension method in another module.
+    public func extensionMethodInAnotherModule() {}
+}
+
+extension Bool {
+    /// An extension method in another module.
+    public func extensionMethodInAnotherModule() {}
+}

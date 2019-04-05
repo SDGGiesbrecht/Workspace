0e3788104ba44c55fdf27af5319b8c1d9d38d26a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-04 22:06:15 -0600

    Refreshed test manifests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,21 +1,6 @@
-/*
- LinuxMain.swift
-
- This source file is part of the Workspace open source project.
- https://github.com/SDGGiesbrecht/Workspace#workspace
-
- Copyright ©2018–2019 Jeremy David Giesbrecht and the Workspace project contributors.
-
- Soli Deo gloria.
-
- Licensed under the Apache Licence, Version 2.0.
- See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
- */
-
 import XCTest
 
 import WorkspaceLibraryTests
-import SDGXCTestUtilities
 
 var tests = [XCTestCaseEntry]()
 tests += WorkspaceLibraryTests.__allTests()

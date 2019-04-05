c3bec71b7efbb703c01bb6ee1d015e6368c7b4e5
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-05 17:50:03 -0600

    ...

--- Tests/Mock Projects/After/SDGLibrary/Tests/LinuxMain.swift
+++ Tests/Mock Projects/After/SDGLibrary/Tests/LinuxMain.swift
@@ -1,17 +1,3 @@
-/*
- LinuxMain.swift
-
- This source file is part of the SDG open source project.
- https://example.github.io/SDG/SDG
-
- Copyright ©2019 John Doe and the SDG project contributors.
-
- Soli Deo gloria.
-
- Licensed under the Apache Licence, Version 2.0.
- See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
- */
-
 import XCTest
 @testable import SDGTests
 

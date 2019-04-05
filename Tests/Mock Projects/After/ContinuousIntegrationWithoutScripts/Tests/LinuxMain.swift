e1d600d7c4f92ebf8f5f9bc725129162a41d0886
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-05 17:43:02 -0600

    ...

--- Tests/Mock Projects/After/ContinuousIntegrationWithoutScripts/Tests/LinuxMain.swift
+++ Tests/Mock Projects/After/ContinuousIntegrationWithoutScripts/Tests/LinuxMain.swift
@@ -1,14 +1,3 @@
-/*
- LinuxMain.swift
-
- This source file is part of the ContinuousIntegrationWithoutScripts open source project.
-
- Copyright ©2019 the ContinuousIntegrationWithoutScripts project contributors.
-
- Licensed under the MIT Licence.
- See https://opensource.org/licenses/MIT for licence information.
- */
-
 import XCTest
 
 import ContinuousIntegrationWithoutScriptsTests

cd4716296930c6a93df34e05b6055c16e9dc918f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-16 23:30:13 -0600

    Updated specification.

--- Tests/Mock Projects/After/ContinuousIntegrationWithoutScripts/Tests/ContinuousIntegrationWithoutScriptsTests/ContinuousIntegrationWithoutScriptsTests.swift
+++ Tests/Mock Projects/After/ContinuousIntegrationWithoutScripts/Tests/ContinuousIntegrationWithoutScriptsTests/ContinuousIntegrationWithoutScriptsTests.swift
@@ -1,9 +1,8 @@
 /*
  ContinuousIntegrationWithoutScriptsTests.swift
 
- This source file is part of the ContinuousIntegrationWithoutScripts open source project.
 
- Copyright ©2019 the ContinuousIntegrationWithoutScripts project contributors.
+ ©2019
 
  Licensed under the MIT Licence.
  See https://opensource.org/licenses/MIT for licence information.

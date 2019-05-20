4a34a7c640f179dd0738c29edb82f3abccc468cf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-20 13:31:08 -0600

    Added dependency on SDGWeb.

--- Package.swift
+++ Package.swift
@@ -140,7 +140,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 18, 0))),
         .package(url: "https://github.com/SDGGiesbrecht/SDGCommandLine", .exact(Version(0, 7, 1))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 10, 0)))
+        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 10, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", .exact(Version(0, 1, 0)))
     ],
     targets: [
         // The executable. (Multiple products duplicate this with localized names.)

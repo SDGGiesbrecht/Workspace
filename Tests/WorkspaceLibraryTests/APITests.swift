0d058d0e3a04ef0df699a91929f4e302d011bf33
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-19 18:26:24 -0600

    Added test.

--- Tests/WorkspaceLibraryTests/APITests.swift
+++ Tests/WorkspaceLibraryTests/APITests.swift
@@ -85,6 +85,7 @@ class APITests : TestCase {
         configuration.documentation.localizations = ["🇬🇧EN", "🇺🇸EN", "🇨🇦EN", "zxx"]
         configuration.documentation.api.generate = true
         configuration.documentation.readMe.about["🇨🇦EN"] = "Stuff about the creators...\n\n...and more stuff..."
+        configuration.documentation.readMe.about["🇺🇸EN"] = "Stuff about the creators...\n\n...and more stuff..."
         configuration.documentation.api.yearFirstPublished = 2018
         configuration.documentation.api.ignoredDependencies.remove("Swift")
         let builtIn = configuration.fileHeaders.copyrightNotice

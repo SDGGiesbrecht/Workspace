a913adbf9e79d8c37140260df3a87f1a24825d39
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-21 18:02:29 -0600

    Removed dead code.

--- Sources/WSOpenSource/PackageRepository.swift
+++ Sources/WSOpenSource/PackageRepository.swift
@@ -23,7 +23,7 @@ import SDGSwiftSource
 
 extension PackageRepository {
 
-    private func refreshReadMe(at location: URL, for localization: LocalizationIdentifier, atProjectRoot: Bool, output: Command.Output) throws {
+    private func refreshReadMe(at location: URL, for localization: LocalizationIdentifier, output: Command.Output) throws {
 
         guard var readMe = try readMe(output: output)[localization] else {
             throw Command.Error(description: UserFacing<StrictString, InterfaceLocalization>({ errorLocalization in
@@ -55,13 +55,6 @@ extension PackageRepository {
                 ].joinedAsLines())
         }
 
-        if ¬atProjectRoot {
-            // Fix links according to location.
-            let prefix = "]("
-            let searchTerm: String = prefix + ReadMeConfiguration._documentationDirectory(for: location).path(relativeTo: location) + "/"
-            readMe.scalars.replaceMatches(for: searchTerm.scalars, with: prefix.scalars)
-        }
-
         var file = try TextFile(possiblyAt: location)
         file.body = String(readMe)
         try file.writeChanges(for: self, output: output)
@@ -69,7 +62,7 @@ extension PackageRepository {
 
     public func refreshReadMe(output: Command.Output) throws {
 
-        try refreshReadMe(at: location.appendingPathComponent("README.md"), for: try developmentLocalization(output: output), atProjectRoot: true, output: output)
+        try refreshReadMe(at: location.appendingPathComponent("README.md"), for: try developmentLocalization(output: output), output: output)
 
         // Deprecated file locations.
         delete(location.appendingPathComponent("Documentation/Related Projects.md"), output: output)

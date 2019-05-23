4bca434191713a602f59450369b73636d6dd5cd0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-22 18:48:21 -0600

    Removed outdated testing hacks.

--- Sources/WSTesting/PackageRepository.swift
+++ Sources/WSTesting/PackageRepository.swift
@@ -110,15 +110,6 @@ extension PackageRepository {
             }
         }).resolved().formattedAsSectionHeader())
 
-        #if TEST_SHIMS
-        if job == .macOS,
-            ProcessInfo.processInfo.environment["__XCODE_BUILT_PRODUCTS_DIR_PATHS"] ≠ nil {
-            // “swift test” gets confused inside Xcode’s test sandbox. This skips it while testing Workspace.
-            output.print("Skipping due to sandbox...")
-            return
-        }
-        #endif
-
         let testCommand: (Command.Output) -> Bool
         switch job {
         case .macOS, .linux:
@@ -190,15 +181,6 @@ extension PackageRepository {
                     return "Updating test manifests..." + section.anchor
                 }
             }).resolved().formattedAsSectionHeader())
-
-            #if TEST_SHIMS
-            if job == .macOS,
-                ProcessInfo.processInfo.environment["__XCODE_BUILT_PRODUCTS_DIR_PATHS"] ≠ nil {
-                // “swift test” gets confused inside Xcode’s test sandbox. This skips it while testing Workspace.
-                output.print("Skipping due to sandbox...")
-                return
-            }
-            #endif
 
             do {
                 switch regenerateTestLists(reportProgress: { output.print($0) }) {

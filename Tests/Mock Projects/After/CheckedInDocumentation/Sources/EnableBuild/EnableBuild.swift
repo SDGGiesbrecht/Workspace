84d101b4f680606651ec6fbd0ee72c4313e46716
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-23 19:21:13 -0600

    Updated specification.

---
+++ Tests/Mock Projects/After/CheckedInDocumentation/Sources/EnableBuild/EnableBuild.swift
@@ -0,0 +1,15 @@
+open class Simple<T> : Hashable {
+    public static func == (lhs: Simple<T>, rhs: Simple<T>) -> Bool { return false }
+    public func hash(into hasher: inout Hasher) {}
+}
+public struct Metatype<T> {}
+public struct BaseType<T> {
+    public struct Member {}
+}
+public struct Unwrapped<T> {}
+public protocol Composition {}
+public struct Type {}
+
+public struct Resources {
+    public static let english = ""
+}

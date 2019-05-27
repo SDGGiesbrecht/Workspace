78b2ee449b337caaf4b56d70557c20a66b7390ae
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-26 18:42:15 -0600

    Moved resources documentation.

--- Sources/WSInterface/Refresh/RefreshResources.swift
+++ Sources/WSInterface/Refresh/RefreshResources.swift
@@ -34,7 +34,33 @@ extension Workspace.Refresh {
             }
         })
 
-        static let command = Command(name: name, description: description, directArguments: [], options: Workspace.standardOptions, execution: { (_, options: Options, output: Command.Output) throws in
+        private static let discussion = UserFacing<StrictString, InterfaceLocalization>({ localization in
+            switch localization {
+            case .englishCanada:
+                return [
+                    "Workspace can embed resources in Swift package targets.",
+                    "",
+                    "Workspace will find and embed any file located within a project’s “Resources” directory. The structure of the “Resources” directory reflects the targets and namespaces under which the resources will be available in Swift code. Immediate subdirectories must correspond to targets; optional deeper nested directories produce namespaces. For example, a project with the following file...",
+                    "",
+                    "Resources/MyLibrary/Templates/Basic Template.txt",
+                    "",
+                    "...can use the file from within the “MyLibrary” target like this...",
+                    "",
+                    "let template: String = Resoures.Templates.basicTemplate",
+                    "print(template)",
+                    "",
+                    "By default, files are embedded as “Data”, but some file extensions will be recognized and given a more specific Swift type (such as “.txt” embedded as “String”).",
+                ].joinedAsLines()
+            }
+        })
+
+        static let command = Command(
+            name: name,
+            description: description,
+            discussion: discussion,
+            directArguments: [],
+            options: Workspace.standardOptions,
+            execution: { (_, options: Options, output: Command.Output) throws in
 
             output.print(UserFacing<StrictString, InterfaceLocalization>({ localization in
                 switch localization {

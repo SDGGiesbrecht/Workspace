3e6ea496914f45c178183c16b3f4386c243f6a05
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-26 19:02:21 -0600

    Moved example documentation.

--- Sources/WSInterface/Refresh/RefreshExamples.swift
+++ Sources/WSInterface/Refresh/RefreshExamples.swift
@@ -34,7 +34,65 @@ extension Workspace.Refresh {
             }
         })
 
-        static let command = Command(name: name, description: description, directArguments: [], options: Workspace.standardOptions, execution: { (_, options: Options, output: Command.Output) throws in
+        private static let discussion = UserFacing<StrictString, InterfaceLocalization>({ localization in
+            switch localization {
+            case .englishCanada:
+                return [
+                    "When APIs change, it is easy to forget to update any examples in the documentation. Workspace allows examples to be synchronized with real, compiled source code in a test module. That way, when an API change makes an example invalid, it will be caught by the compiler.",
+                    "",
+                    "Examples can be defined anywhere in the project, but usually the best place for them is in a test module.",
+                    "",
+                    "To define an example, place it between “@example(identifier)” and “@endExample”. Anything on the same line as either token will be ignored (such as “//”).",
+                    "",
+                    "func forTheSakeOfLeavingTheGlobalScope() {",
+                    "    let a = 0",
+                    "    let b = 0",
+                    "    let c = 0",
+                    "",
+                    "    // @example(symmetry)",
+                    "    if a == b {",
+                    "        assert(b == a)",
+                    "    }",
+                    "    // @endExample",
+                    "",
+                    "    // @example(transitivity)",
+                    "    if a == b ∧ b == c {",
+                    "        assert(a == c)",
+                    "    }",
+                    "    // @endExample",
+                    "}",
+                    "",
+                    "To use an example in a symbol’s documentation, add one or more instances of “#example(0, identifier)” to the line immediately preceding the documentation.",
+                    "",
+                    "// #example(1, symmetry) #example(2, transitivity)",
+                    "/// Returns `true` if `lhs` is equal to `rhs`.",
+                    "///",
+                    "/// Equality is symmetrical:",
+                    "///",
+                    "/// ```swift",
+                    "/// (Workspace will automatically fill these in whenever the project is refreshed.)",
+                    "/// ```",
+                    "///",
+                    "/// Equality is transitive:",
+                    "///",
+                    "/// ```swift",
+                    "///",
+                    "/// ```",
+                    "func == (lhs: Thing, rhs: Thing) -> Bool {",
+                    "    return lhs.rawValue == rhs.rawValue",
+                    "}",
+                    ].joinedAsLines()
+            }
+        })
+
+
+        static let command = Command(
+            name: name,
+            description: description,
+            discussion: discussion,
+            directArguments: [],
+            options: Workspace.standardOptions,
+            execution: { (_, options: Options, output: Command.Output) throws in
 
             output.print(UserFacing<StrictString, InterfaceLocalization>({ localization in
                 switch localization {

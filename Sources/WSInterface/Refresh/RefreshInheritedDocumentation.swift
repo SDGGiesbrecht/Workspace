ecfabc29089cbd4c5d826b6b492e51d206cdfadd
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-26 18:53:41 -0600

    Moved documentation inheritance documentation.

--- Sources/WSInterface/Refresh/RefreshInheritedDocumentation.swift
+++ Sources/WSInterface/Refresh/RefreshInheritedDocumentation.swift
@@ -34,7 +34,47 @@ extension Workspace.Refresh {
             }
         })
 
-        static let command = Command(name: name, description: description, directArguments: [], options: Workspace.standardOptions, execution: { (_, options: Options, output: Command.Output) throws in
+        private static let discussion = UserFacing<StrictString, InterfaceLocalization>({ localization in
+            switch localization {
+            case .englishCanada:
+                return [
+                    "It can be tedious re‐writing the same documentation over again. Workspace can make documentation comments re‐usable.",
+                    "",
+                    "Note: Both Xcode and Workspace now do this automatically in many situations when the child symbol is left undocumented, such as protocol conformances, default implementations and subclass overrides. The explicit directives described here should only be used as a fall‐back in situations where the tools cannot deduce the parent symbol automatically.",
+                    "",
+                    "To designate a documentation comment as a definition, place “@documentation(identifier)” on the line above. Anything on the same line will be ignored (such as “//”).",
+                    "",
+                    "protocol Rambler {",
+                    "    // @documentation(MyLibrary.Rambler.ramble)",
+                    "    /// Rambles on and on and on and on...",
+                    "    func ramble() -> Never",
+                    "}",
+                    "",
+                    "Workspace can find definitions in any Swift file in the project.",
+                    "",
+                    "To inherit the documentation elsewhere, place “#documentation(identifier)” where the documentation would go (or above it if it already exists). Anything on the same line will be ignored (such as “//”).",
+                    "",
+                    "struct Teacher : Rambler {",
+                    "    // #documentation(MyLibrary.Rambler.ramble)",
+                    "    /// (Workspace will automatically fill this in whenever the project is refreshed.)",
+                    "    func ramble() -> Never {",
+                    "        print(\u{22}Blah\u{22})",
+                    "        while true {",
+                    "            print(\u{22}, blah\u{22})",
+                    "        }",
+                    "    }",
+                    "}",
+                    ].joinedAsLines()
+            }
+        })
+
+        static let command = Command(
+            name: name,
+            description: description,
+            discussion: discussion,
+            directArguments: [],
+            options: Workspace.standardOptions,
+            execution: { (_, options: Options, output: Command.Output) throws in
 
             output.print(UserFacing<StrictString, InterfaceLocalization>({ localization in
                 switch localization {

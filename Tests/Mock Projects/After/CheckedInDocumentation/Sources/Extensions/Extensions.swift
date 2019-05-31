9d8b0f55e0b9099bdb04e854535fbe0563620533
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-31 16:45:48 -0600

    Updated specifications.

--- Tests/Mock Projects/After/CheckedInDocumentation/Sources/Extensions/Extensions.swift
+++ Tests/Mock Projects/After/CheckedInDocumentation/Sources/Extensions/Extensions.swift
@@ -1,16 +1,19 @@
 import CheckedInDocumentation
 
 extension Structure {
+    // @localization(🇨🇦EN) @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(zxx)
     /// An extension method in another module.
     public func extensionMethodInAnotherModule() {}
 }
 
 extension Protocol {
+    // @localization(🇨🇦EN) @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(zxx)
     /// An extension method in another module.
     public func extensionMethodInAnotherModule() {}
 }
 
 extension Bool {
+    // @localization(🇨🇦EN) @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(zxx)
     /// An extension method in another module.
     public func extensionMethodInAnotherModule() {}
 }

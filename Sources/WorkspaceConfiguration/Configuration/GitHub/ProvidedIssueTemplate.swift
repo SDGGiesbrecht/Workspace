3a8cee53b69b4e4f2a634f0f73429f86d5f2e235
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-06 15:48:30 -0600

    Refactored.

--- Sources/WorkspaceConfiguration/Configuration/GitHub/ProvidedIssueTemplate.swift
+++ Sources/WorkspaceConfiguration/Configuration/GitHub/ProvidedIssueTemplate.swift
@@ -24,8 +24,37 @@ internal enum ProvidedIssueTemplate: CaseIterable {
     // MARK: - Construction
 
     internal func constructed(for localization: ContentLocalization) -> IssueTemplate {
-        #warning("Update this.")
-        var template = StrictString(Resources.issueTemplate)
+        var contents: [StrictString] = []
+        contents.append(contentsOf: [
+            "<!--",
+            " Reminder:",
+            " Have you searched to see if a related issue exists already?",
+            " If one exists, please add your information there instead.",
+            " -->",
+            "",
+            "### Description",
+            "",
+            "“Such‐and‐such appears broken.”",
+            "or",
+            "“Such‐and‐such would be a nice feature.”",
+            "",
+            "### Demonstration",
+            "<!-- (If the issue is not a bug, erase this section.) -->",
+            "",
+            "#trigger",
+            "",
+            "### Availability to Help",
+            "",
+            "<!-- Keep only one of the following lines. -->",
+            "I **would like** the honour of helping with the implementation, and I think **I know my way around**.",
+            "I **would like** the honour of helping with the implementation, but **I would need some guidance** along the way.",
+            "I **do not want to help** with the implementation.",
+            "",
+            "### Solution/Design Thoughts",
+            "",
+            "It might work to do something like..."
+            ])
+        var template = contents.joinedAsLines()
 
         let products = WorkspaceContext.current.manifest.products
         var trigger: [StrictString] = []

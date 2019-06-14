4b0bbd1d9ae35576bcd6f0686ae4178af0911f47
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-13 21:17:54 -0600

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -141,7 +141,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(1, 0, 0))),
         .package(url: "https://github.com/SDGGiesbrecht/SDGCommandLine", .exact(Version(1, 0, 0))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 12, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .exact(Version(0, 12, 1))),
         .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", .exact(Version(1, 0, 0)))
     ],
     targets: [

9632d8a4af59ec83a6840d82f28c106afbce349e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-08-06 17:33:21 -0600

    ...

--- Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Deprecation/InheritanceDirectives.swift
+++
@@ -1,8 +0,0 @@
-// Header
-
-// [_Define Documentation: This inheritance style is deprecated._]
-/// ...
-func base() {}
-
-// [_Inherit Documentation: This inheritance style is deprecated._]
-func inherited() {}
--- Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Deprecation/Warnings.swift
+++
@@ -1,4 +0,0 @@
-// Header
-
-// [_Warning: This warning style is deprecated._]
-// [_Workaround: This workaround style is deprecated._]

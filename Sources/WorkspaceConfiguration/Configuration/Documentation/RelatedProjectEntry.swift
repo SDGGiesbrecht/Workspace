2d9def96e6e5b9b3b9b7b658c78ba5a436e1f161
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-08-06 17:54:49 -0600

    ...

--- Sources/WorkspaceConfiguration/Configuration/Documentation/RelatedProjectEntry.swift
+++ Sources/WorkspaceConfiguration/Configuration/Documentation/RelatedProjectEntry.swift
@@ -2,9 +2,11 @@
  RelatedProjectEntry.swift
 
  This source file is part of the Workspace open source project.
+ Diese Quelldatei ist Teil des qeulloffenen Workspace‐Projekt.
  https://github.com/SDGGiesbrecht/Workspace#workspace
 
  Copyright ©2018–2019 Jeremy David Giesbrecht and the Workspace project contributors.
+ Urheberrecht ©2018–2019 Jeremy David Giesbrecht und die Mitwirkenden des Workspace‐Projekts.
 
  Soli Deo gloria.
 

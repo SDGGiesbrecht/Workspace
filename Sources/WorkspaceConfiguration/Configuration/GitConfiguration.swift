311f5a92d06205db19bd10c9f8e32b972c58f4ee
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-08-12 16:31:46 -0600

    ...

--- Sources/WorkspaceConfiguration/Configuration/GitConfiguration.swift
+++ Sources/WorkspaceConfiguration/Configuration/GitConfiguration.swift
@@ -14,20 +14,35 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-// #workaround(Not properly localized yet.)
-// @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(🇨🇦EN) @localization(🇩🇪DE)
+// @localization(🇩🇪DE) @crossReference(GitConfiguration)
+/// Einstellungen zu Git.
+public typealias GitEinstellungen = GitConfiguration
+// @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(🇨🇦EN) @crossReference(GitConfiguration)
 /// Options related to Git.
 public struct GitConfiguration : Codable {
 
-    // #workaround(Not properly localized yet.)
-    // @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(🇨🇦EN) @localization(🇩🇪DE)
+    // @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(🇨🇦EN) @crossReference(GitConfiguration.manage)
     /// Whether or not to manage the project’s Git configuration files.
     ///
     /// This is off by default.
     public var manage: Bool = false
+    // @localization(🇩🇪DE) @crossReference(GitConfiguration.manage)
+    /// Ob Arbeitsbereich die Git‐Konfigurationsdateien des Projekts verwalten soll.
+    ///
+    /// Wenn nicht angegeben, ist diese Einstellung aus.
+    public var verwalten: Bool {
+        get { return manage }
+        set { manage = newValue }
+    }
 
-    // #workaround(Not properly localized yet.)
-    // @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(🇨🇦EN) @localization(🇩🇪DE)
+    // @localization(🇬🇧EN) @localization(🇺🇸EN) @localization(🇨🇦EN)
+    // @crossReference(GitConfiguration.additionalGitIgnoreEntries)
     /// Additional entries to append to the standard gitignore file.
     public var additionalGitIgnoreEntries: [String] = []
+    // @localization(🇩🇪DE) @crossReference(GitConfiguration.additionalGitIgnoreEntries)
+    /// Weitere Einträge, die Arbeitsbereich zu der Standard‐Git‐Übergehen‐Datei.
+    public var weitereEinträgeZuGitÜbergehen: [Zeichenkette] {
+        get { return additionalGitIgnoreEntries }
+        set { additionalGitIgnoreEntries = newValue }
+    }
 }

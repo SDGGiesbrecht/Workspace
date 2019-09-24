b9bebd68380bc5fa2d341d9351d0be4bc992ae70
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-23 18:49:54 -0600

    ...

--- Tests/WorkspaceLibraryTests/APITests.swift
+++ Tests/WorkspaceLibraryTests/APITests.swift
@@ -629,9 +629,13 @@ class APITests : TestCase {
         configuration.documentation.localizations = ["en"]
         configuration.documentation.currentVersion = Version(1, 0, 0)
         configuration.documentation.repositoryURL = URL(string: "https://somewhere.tld/repository")!
-        PackageRepository(mock: "MultipleProducts").test(commands: [
-            ["refresh", "read‐me"]
-            ], configuration: configuration, localizations: FastTestLocalization.self, overwriteSpecificationInsteadOfFailing: false)
+        PackageRepository(mock: "MultipleProducts").test(
+            commands: [
+                ["refresh", "read‐me"]
+            ],
+            configuration: configuration,
+            localizations: FastTestLocalization.self,
+            overwriteSpecificationInsteadOfFailing: false)
     }
 
     func testNoLibraries() {

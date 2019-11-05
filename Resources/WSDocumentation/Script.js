f5954f37fb482235bc4c0c48e3fed465e2cc63aa
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-04 19:28:48 -0600

    Updated JavaScript.

--- Resources/WSDocumentation/Script.js
+++ Resources/WSDocumentation/Script.js
@@ -42,16 +42,18 @@ function toggleIndexSectionVisibility(sender) {
     }
 }
 
-function contractIndex() {
+function contractIndex(currentSection) {
     var indexElements = document.getElementsByClassName("index");
     var index = indexElements.item(indexElements.length - 1);
     var sections = index.children;
     for (var sectionIndex = 1; sectionIndex < sections.length; sectionIndex++) {
         var section = sections[sectionIndex]
-        var links = section.children
-        for (var linkIndex = 1; linkIndex < links.length; linkIndex++) {
-            var link = links[linkIndex]
-            toggleLinkVisibility(link)
+        if (section.getAttribute('id') != currentSection) {
+            var links = section.children
+            for (var linkIndex = 1; linkIndex < links.length; linkIndex++) {
+                var link = links[linkIndex]
+                toggleLinkVisibility(link)
+            }
         }
     }
 }

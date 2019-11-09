e5c2af55c2f0027c00dbf14e967d6ee8d758063b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-08 18:44:14 -0600

    ...

--- Tests/Mock Projects/After/CustomProofread/Sources/CustomProofread/CustomProofread.swift
+++ Tests/Mock Projects/After/CustomProofread/Sources/CustomProofread/CustomProofread.swift
@@ -8,5 +8,7 @@
  See http://www.gnu.org/licenses/ for licence information.
  */
 
-let badlySpacedColon :Bool = true // This should be allowed because colon spacing is disabled.
-let string = "a && b" // This should be allowed because ambiguous scope is disabled.
+/// ...
+///
+/// - warning: This should be allowed because callout casing is disabled.
+let string = "a && b"  // This should be allowed because ambiguous scope is disabled.

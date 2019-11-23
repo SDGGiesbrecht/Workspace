4f609f991741a21fca513179ec4e46b0ac2de9c7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-22 19:00:48 -0600

    Refreshed.

--- Sources/WSContinuousIntegration/PackageRepository.swift
+++ Sources/WSContinuousIntegration/PackageRepository.swift
@@ -71,8 +71,8 @@ extension PackageRepository {
     ]
 
     for job in try relevantJobs(output: output)
-      // #workaround(Activating one at a time.)
-      where job ∉ Set([.linux, .iOS, .watchOS, .tvOS, .miscellaneous, .deployment]) {
+    // #workaround(Activating one at a time.)
+    where job ∉ Set([.linux, .iOS, .watchOS, .tvOS, .miscellaneous, .deployment]) {
       workflow.append(contentsOf: job.gitHubWorkflowJob(configuration: configuration))
     }
 

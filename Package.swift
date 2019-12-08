2e6230a346fc1492200deeaeeaa8d9940c7003d3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-08 15:23:39 -0600

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -215,7 +215,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/SDGSwift",
-      .upToNextMinor(from: Version(0, 18, 1))
+      .upToNextMinor(from: Version(0, 18, 2))
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/swift\u{2D}format",

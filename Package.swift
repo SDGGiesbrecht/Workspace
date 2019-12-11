60aab17a6280486a6ab86a1d35293e85ace6c734
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-11 14:55:29 -0600

    ...

--- Package.swift
+++ Package.swift
@@ -215,9 +215,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/SDGSwift",
-      // #warning(Branch dependency.)
-      .branch("master")
-      //.upToNextMinor(from: Version(0, 18, 2))
+      .upToNextMinor(from: Version(0, 18, 4))
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/swift\u{2D}format",

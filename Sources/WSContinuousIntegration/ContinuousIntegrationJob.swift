897268669882d86d73b9694592b594cbc32f6846
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-11 21:05:13 -0600

    Updated Swift and Xcode in CI.

--- Sources/WSContinuousIntegration/ContinuousIntegrationJob.swift
+++ Sources/WSContinuousIntegration/ContinuousIntegrationJob.swift
@@ -33,8 +33,8 @@ public enum ContinuousIntegrationJob: Int, CaseIterable {
   case miscellaneous
   case deployment
 
-  public static let currentSwiftVersion = Version(5, 1, 1)
-  public static let currentXcodeVersion = Version(11, 2, 0)
+  public static let currentSwiftVersion = Version(5, 1, 2)
+  public static let currentXcodeVersion = Version(11, 3, 0)
 
   public static let simulatorJobs: Set<ContinuousIntegrationJob> = [
     .iOS,

2272b10474fd755bf0c7ffa2e219571ff591c6ae
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-27 13:19:13 -0600

    Fixed style.

--- Sources/WSContinuousIntegration/ContinuousIntegrationJob.swift
+++ Sources/WSContinuousIntegration/ContinuousIntegrationJob.swift
@@ -343,7 +343,7 @@ public enum ContinuousIntegrationJob: Int, CaseIterable {
         "        git config user.email \u{22}${GITHUB_ACTOR}@users.noreply.github.com\u{22}",
         "        git add .",
         "        git commit \u{2D}m \u{22}\(deployCommitMessage.resolved(for: interfaceLocalization))\u{22}",
-        "        git push \u{2D}\u{2D}force https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git master:gh\u{2D}pages",
+        "        git push \u{2D}\u{2D}force https://x\u{2D}access\u{2D}token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git master:gh\u{2D}pages",
         "      env:",
         "        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}"
       ])

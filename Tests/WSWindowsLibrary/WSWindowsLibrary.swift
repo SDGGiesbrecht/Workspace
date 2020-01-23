937f55c74b53378b9a0af7f55365c81985b2c72d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-22 23:50:37 -0600

    Imported FoundationNetworking and FoundationXML on Windows.

--- Tests/WSWindowsLibrary/WSWindowsLibrary.swift
+++ Tests/WSWindowsLibrary/WSWindowsLibrary.swift
@@ -15,12 +15,21 @@
  */
 
 import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+#if canImport(FoundationXML)
+  import FoundationXML
+#endif
+
 import Dispatch
 
 public func helloWorld() {
   // @exempt(from: tests) #workaround(Not testable yet.)
   print("Hello, world!")
   print(NSString(string: "Hello, Foundation!"))
+  print(URLCredential(user: "Hello,", password: "FoundationNetworking", persistence: .none))
+  print(XMLElement(name: "Hello, FoundationXML!"))
   print(DispatchQueue(label: "Hello, Dispatch!"))
 }
 

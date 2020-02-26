d8a796b9b00d65131a60c43a5cb32e36908d58d7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-25 19:04:16 -0600

    Adjusted directory.

--- Tests/WSAndroidTests/WSAndroidTests.swift
+++ Tests/WSAndroidTests/WSAndroidTests.swift
@@ -45,9 +45,16 @@ final class AndroidTests: XCTestCase {
   }
 
   func testCachePermissions() throws {
-    let directory = FileManager.default.url(in: .cache, at: "Directory")
+    var directory = FileManager.default.url(in: .cache, at: "Directory")
     #warning("Debugging...")
     print("Got directory:", directory.path)
+    if directory.path.hasPrefix("//") {
+      var repaired = directory.path
+      repaired.removeFirst()
+      directory = URL(fileURLWithPath: repaired)
+    }
+    #warning("Debugging...")
+    print("Repaired directory:", directory.path)
     try? FileManager.default.removeItem(at: directory)
     defer { try? FileManager.default.removeItem(at: directory) }
 

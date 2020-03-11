6951be05cf792d01d95c860809d82a9d6544dce8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-10 22:48:53 -0600

    Fixed WindowsMain formatting.

--- Tests/WSCrossPlatformTests/CrossPlatformTests.swift
+++ Tests/WSCrossPlatformTests/CrossPlatformTests.swift
@@ -33,6 +33,11 @@ final class CrossPlatformTests: TestCase {
     try "text".save(to: directory.appendingPathComponent("Text.txt"))
   }
 
+  func
+    testReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyReallyLongTestName(
+    )
+  {}
+
   func testRepositoryPresence() throws {
     compare(
       "Android",

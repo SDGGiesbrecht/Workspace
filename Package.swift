e8754a1dd573f04c3d71b768485889d80dd567da
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-01 13:27:58 -0600

    Disabled more web dependencies.

--- Package.swift
+++ Package.swift
@@ -855,7 +855,8 @@ func adjustForWeb() {
     "SDGWeb",
     "swift\u{2D}format",
     "swift\u{2D}package\u{2D}manager",
-    "swift\u{2D}tools\u{2D}support\u{2D}core",
+    "swift\u{2D}syntax",
+    "swift\u{2D}tools\u{2D}support\u{2D}core.git",
   ]
   package.dependencies.removeAll(where: { dependency in
     for impossible in impossiblePackages {

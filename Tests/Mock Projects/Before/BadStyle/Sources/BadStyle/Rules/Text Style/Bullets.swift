990dfab6158a5968fc6017789986e77f447febd1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-03 13:31:58 -0600

    Added test.

---
+++ Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Text Style/Bullets.swift
@@ -0,0 +1,10 @@
+// Header
+
+/// Documentation.
+///
+/// - Should not trigger; this is the right kind of bullet.
+///
+/// * Should trigger; asterisks aren’t bullets.
+///
+/// + Should trigger; plus signs aren’t bullets.
+func testBullets() {}

ff1ddf3446ddbb2ea11118815df94fb32ecfa865
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-04 17:39:21 -0600

    Added test.

---
+++ Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Source Code Style/Bullets.swift
@@ -0,0 +1,12 @@
+// Header
+
+/// Documentation.
+///
+/// # Should not trigger; this is the right kind of heading.
+///
+/// Should trigger; underlines may not align.
+/// -----------------------------------------
+///
+/// Should trigger; underlines may not align.
+/// =========================================
+func testHeadings() {}

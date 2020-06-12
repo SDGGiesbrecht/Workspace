fd314b1bf5f19e8c8a1aff02f24c87da478522c2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-11 21:43:01 -0600

    Disabled test.

--- HelloWorld/Package.swift
+++ HelloWorld/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
   products: [
     .library(
       name: "HelloWorld",
-      targets: ["HelloWorld"]),
+      targets: ["HelloWorld"])
   ],
   targets: [
     .target(
@@ -16,8 +16,8 @@ let package = Package(
     .target(
       name: "HelloC",
       dependencies: []),
-    .testTarget(
+    /*.testTarget(
       name: "HelloWorldTests",
-      dependencies: ["HelloWorld"]),
+      dependencies: ["HelloWorld"])*/
   ]
 )

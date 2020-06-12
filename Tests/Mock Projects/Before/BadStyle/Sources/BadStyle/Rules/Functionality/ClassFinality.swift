fc3646dd370145ec9b0e6e45b514022c9a2340d4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-12 14:01:15 -0600

    Updated specification.

---
+++ Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Functionality/ClassFinality.swift
@@ -0,0 +1,8 @@
+public class Class {} // Should warn; neither open nor final.
+
+open class Open {} // Should not warn; open.
+
+public final class Final {} // Should not warn; final.
+
+internal class Internal {} // Should not warn; compiler can infer finality.
+class ImplicitInternal {} // Should not warn; compiler can infer finality.

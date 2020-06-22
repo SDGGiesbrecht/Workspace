0a279c9b1ad2a245b2fd478fd8cd3f40ac9c9025
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-21 19:41:43 -0600

    Adjusted tests.

--- Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Text Style/Bullets.swift
+++ Tests/Mock Projects/Before/BadStyle/Sources/BadStyle/Rules/Text Style/Bullets.swift
@@ -7,4 +7,6 @@
 /// * Should trigger; asterisks aren’t bullets.
 ///
 /// + Should trigger; plus signs aren’t bullets.
+///
+/// 1. Should not trigger; ordered lists are okay.
 func testBullets() {}

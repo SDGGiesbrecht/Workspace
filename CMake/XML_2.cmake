89a0c2e616c0c31ba5100e28d781ec7d7aac836d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-23 15:56:55 -0600

    Added XML 2.

---
+++ CMake/XML_2.cmake
@@ -0,0 +1,9 @@
+# Based on https://github.com/compnerd/swift-build/blob/master/cmake/caches/libxml2.cmake
+
+set(LIBXML2_WITH_ICONV NO CACHE BOOL "")
+
+set(LIBXML2_WITH_PYTHON NO CACHE BOOL "")
+
+set(LIBXML2_WITH_LZMA NO CACHE BOOL "")
+
+set(LIBXML2_WITH_ZLIB NO CACHE BOOL "")

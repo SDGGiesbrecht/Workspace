b19f1cee26fcef9ce2690e7ac1b62573abc1e0bb
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-07-30 17:23:20 -0600

    Tried without test discovery.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,15 +1,10 @@
-/*
- LinuxMain.swift
+import XCTest
 
- This source file is part of the Workspace open source project.
- Diese Quelldatei ist Teil des quelloffenen Arbeitsbereich‐Projekt.
- https://github.com/SDGGiesbrecht/Workspace#workspace
+import WSCrossPlatformTests
+import WorkspaceLibraryTests
 
- Copyright ©2019–2020 Jeremy David Giesbrecht and the Workspace project contributors.
- Urheberrecht ©2019–2020 Jeremy David Giesbrecht und die Mitwirkenden des Arbeitsbereich‐Projekts.
+var tests = [XCTestCaseEntry]()
+tests += WSCrossPlatformTests.__allTests()
+tests += WorkspaceLibraryTests.__allTests()
 
- Soli Deo gloria.
-
- Licensed under the Apache Licence, Version 2.0.
- See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
- */
+XCTMain(tests)

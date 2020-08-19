0d214bcbc6b94c30913d0d89835002f6ae34a8c2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-08-18 22:35:13 -0600

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -260,7 +260,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/SDGSwift",
-      from: Version(2, 1, 0)
+      from: Version(2, 1, 1)
     ),
     .package(
       name: "SwiftPM",

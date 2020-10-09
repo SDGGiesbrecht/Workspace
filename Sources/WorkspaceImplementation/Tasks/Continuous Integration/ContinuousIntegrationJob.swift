4b0ae776d45a242371e21987d08d3b800fb5244a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-09 10:20:16 -0600

    Generated.

--- Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
+++ Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
@@ -885,6 +885,7 @@ internal enum ContinuousIntegrationJob: Int, CaseIterable {
             heading: installSwiftStepName,
             localization: interfaceLocalization,
             commands: [
+              "sudo rm \u{2D}rf /usr/lib/clang/10.0.0",
               cURL(
                 "https://swift.org/builds/swift\u{2D}\(version)\u{2D}release/ubuntu\(ubuntuVersion.replacingMatches(for: ".", with: ""))/swift\u{2D}\(version)\u{2D}RELEASE/swift\u{2D}\(version)\u{2D}RELEASE\u{2D}ubuntu\(ubuntuVersion).tar.gz",
                 andUntarTo: "/",

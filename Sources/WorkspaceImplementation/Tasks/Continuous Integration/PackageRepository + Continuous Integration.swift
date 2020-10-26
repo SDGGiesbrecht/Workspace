add15f25e463add4d647974384b16f4da82f4185
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-25 19:22:30 -0600

    Updated generator.

--- Sources/WorkspaceImplementation/Tasks/Continuous Integration/PackageRepository + Continuous Integration.swift
+++ Sources/WorkspaceImplementation/Tasks/Continuous Integration/PackageRepository + Continuous Integration.swift
@@ -121,7 +121,7 @@ import WorkspaceConfiguration
       try cleanCMakeUp(output: output)
       try refreshWindowsTests(output: output)
       try refreshWindowsSDK(output: output)
-      try refreshAndroidSDK(output: output)
+      try cleanAndroidSDKUp(output: output)
     }
 
     private func adjustForWorkspace(_ configuration: inout [StrictString]) throws {
@@ -316,27 +316,9 @@ import WorkspaceConfiguration
       }
     }
 
-    private func refreshAndroidSDK(output: Command.Output) throws {
+    private func cleanAndroidSDKUp(output: Command.Output) throws {
       let url = location.appendingPathComponent(".github/workflows/Android/SDK.json")
-      if try ¬relevantJobs(output: output).contains(.android) {
-        delete(url, output: output)
-      } else {
-        let sdk: [String] = [
-          "{",
-          "  \u{22}version\u{22}: 1,",
-          "  \u{22}sdk\u{22}: \u{22}/Library/Developer/Platforms/Android.platform/Developer/SDKs/Android.sdk\u{22},",
-          "  \u{22}toolchain\u{2D}bin\u{2D}dir\u{22}: \u{22}/Library/Developer/Toolchains/unknown\u{2D}Asserts\u{2D}development.xctoolchain/usr/bin\u{22},",
-          "  \u{22}target\u{22}: \u{22}x86_64\u{2D}unknown\u{2D}linux\u{2D}android\u{22},",
-          "  \u{22}dynamic\u{2D}library\u{2D}extension\u{22}: \u{22}so\u{22},",
-          "  \u{22}extra\u{2D}cc\u{2D}flags\u{22}: [],",
-          "  \u{22}extra\u{2D}swiftc\u{2D}flags\u{22}: [],",
-          "  \u{22}extra\u{2D}cpp\u{2D}flags\u{22}: []",
-          "}",
-        ]
-        var sdkFile = try TextFile(possiblyAt: url)
-        sdkFile.contents = sdk.joinedAsLines()
-        try sdkFile.writeChanges(for: self, output: output)
-      }
+      delete(url, output: output)
     }
   }
 #endif

1d3cd0d4d63f3288a4373f8990a5d2ffeb610bb1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-11-05 14:23:54 -0600

    Removed Git workaround.

--- Sources/WorkspaceImplementation/Shared/Project/PackageRepository.swift
+++ Sources/WorkspaceImplementation/Shared/Project/PackageRepository.swift
@@ -475,15 +475,6 @@ import WorkspaceProjectConfiguration
 
           var ignoredURLs: [URL] = try ignoredFiles().get()
           ignoredURLs.append(location.appendingPathComponent(".git"))
-          // #workaround(SDGSwift 3.0.0, Git started quoting “Validate (macOS).command” and SDGSwift doesn’t catch it yet.)
-          ignoredURLs = Array(
-            ignoredURLs.map({ url in
-              return [
-                url,
-                URL(fileURLWithPath: url.path.replacingOccurrences(of: "\u{22}", with: "")),
-              ]
-            }).joined()
-          )
 
           let result = try allFiles().filter { url in
             for ignoredURL in ignoredURLs {

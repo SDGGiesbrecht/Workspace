4c1c7f8cad5ab464132e8e5dac629d92f74c7f3e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-11-14 21:14:21 -0600

    Resumed development.

--- Workspace.swift
+++ Workspace.swift
@@ -20,7 +20,7 @@ public enum Metadata {
 
   public static let latestStableVersion = Version(0, 35, 3)
   // Set this to latestStableVersion for release commits, nil the rest of the time.
-  public static let thisVersion: Version? = latestStableVersion
+  public static let thisVersion: Version? = nil
 
   // #workaround(SDGCornerstone 6.1.0, Web API incomplete.)
   #if !os(WASI)

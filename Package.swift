148e1c5cbae5d469ec50677f357b402903794711
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-06 19:12:26 -0600

    Updated workaround.

--- Package.swift
+++ Package.swift
@@ -793,7 +793,7 @@ for target in package.targets {
     .define("PLATFORM_LACKS_DISPATCH", .when(platforms: [.wasi])),
     // #workaround(Swift 5.4.2, Web lacks Foundation.FileManager.)
     .define("PLATFORM_LACKS_FOUNDATION_FILE_MANAGER", .when(platforms: [.wasi])),
-    // #warning(Swift 5.4.2, Web lacks Foundation.Process.)
+    // #workaround(Swift 5.4.2, Web lacks Foundation.Process.)
     .define("PLATFORM_LACKS_FOUNDATION_PROCESS_INFO", .when(platforms: [.wasi])),
     // #workaround(Swift 5.4.2, FoundationXML is broken on Windows.)
     // #workaround(Swift 5.4.2, FoundationXML is broken on web.)

990a140fc219069d7000b278037ace7d2fb9def2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-11-28 14:17:08 -0600

    Updated Carton.

--- Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
+++ Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
@@ -50,7 +50,7 @@
     private static let currentWindowsVersion = "10"
     private static let currentVisualStudioVersion = "2019"
     private static let currentWSLImage = "2004"
-    private static let currentCartonVersion = Version(0, 11, 1)
+    private static let currentCartonVersion = Version(0, 12, 0)
     private static let currentCentOSVersion = "8"
     private static let currentUbuntuName = "focal"  // Used by Docker image
     private static let currentUbuntuVersion = "20.04"  // Used by GitHub host

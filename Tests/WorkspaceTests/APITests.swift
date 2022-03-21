29aca3cf2922917fc41b1acef84344c74d6d0e1a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-03-20 22:16:45 -0600

    Debugging...

--- Tests/WorkspaceTests/APITests.swift
+++ Tests/WorkspaceTests/APITests.swift
@@ -64,6 +64,11 @@
       APITests.configureGit
     }
 
+    func testCompiler() {
+      #warning("Debugging...")
+      print(SwiftCompiler.version(forConstraints: Version(5, 5, 0)...Version(5, 6, 0)))
+    }
+
     func testAllDisabled() {
       #if !os(Windows)  // #workaround(Swift 5.3.3, SegFault)
         let configuration = WorkspaceConfiguration()

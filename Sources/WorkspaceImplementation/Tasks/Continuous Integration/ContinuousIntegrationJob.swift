333c5ab9c07de537a2bb25bd26c88801240ae1da
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-05-27 22:54:49 -0600

    Updated Carton.

--- Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
+++ Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
@@ -46,7 +46,7 @@
     private static let currentMacOSVersion = Version(12)
     internal static let currentXcodeVersion = Version(13, 4)
     private static let currentVisualStudioVersion = "2019"
-    private static let currentCartonVersion = Version(0, 14, 2)
+    private static let currentCartonVersion = Version(0, 15, 3)
     private static let currentUbuntuName = "focal"  // Used by Docker image
     private static let currentUbuntuVersion = "20.04"  // Used by GitHub host
     private static let currentAnroidNDKVersion = "23b"

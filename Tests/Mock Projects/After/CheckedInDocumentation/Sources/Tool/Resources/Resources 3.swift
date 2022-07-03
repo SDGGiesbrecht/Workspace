e3ddac9a97c86c82a39621cecc179267d6a76953
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-07-03 16:20:35 -0600

    Updated specification.

---
+++ Tests/Mock Projects/After/CheckedInDocumentation/Sources/Tool/Resources/Resources 3.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension Resources.Namespace {
+  static var namespaced: String {
+    return String(data: Data(([] as [[UInt8]]).lazy.joined()), encoding: String.Encoding.utf8)!
+  }
+}

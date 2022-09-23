671c29506a9c3c9c664e02bb29544ba1a3e2b12a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-09-23 15:05:50 -0600

    Updated Swift for Windows.

--- Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
+++ Sources/WorkspaceImplementation/Tasks/Continuous Integration/ContinuousIntegrationJob.swift
@@ -42,7 +42,6 @@
     case deployment
 
     internal static let currentSwiftVersion = Version(5, 7, 0)
-    internal static let currentSwiftVersionForWindows = Version(5, 6, 0)
     internal static let currentSwiftVersionForAndroid = Version(5, 6, 0)
 
     private static let currentMacOSVersion = Version(12)
@@ -723,7 +722,7 @@
           )
         ])
       case .windows:
-        let version = ContinuousIntegrationJob.currentSwiftVersionForWindows
+        let version = ContinuousIntegrationJob.currentSwiftVersion
           .string(droppingEmptyPatch: true)
         result.append(contentsOf: [
           step(installSwiftStepName, localization: interfaceLocalization),

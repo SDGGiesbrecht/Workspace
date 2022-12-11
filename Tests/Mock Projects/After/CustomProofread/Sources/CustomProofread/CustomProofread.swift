c5d16a04e5a5975ac8ae2322c82f5f8f734e29d7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-12-10 21:10:52 -0600

    ...

--- Tests/Mock Projects/After/CustomProofread/Sources/CustomProofread/CustomProofread.swift
+++ Tests/Mock Projects/After/CustomProofread/Sources/CustomProofread/CustomProofread.swift
@@ -11,4 +11,5 @@
 /// ...
 ///
 /// - warning: This should be allowed because callout casing is disabled.
-public let string = "a && b"  // This should be allowed because ambiguous scope is disabled.
+public let string: String =
+  "a && b"  // This should be allowed because ambiguous scope is disabled.

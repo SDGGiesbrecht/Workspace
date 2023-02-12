4bd7e8bcb4b9bb596bf0009f6af34e1bd19a5381
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-02-12 14:21:52 -0600

    Refreshed copyright.

--- Tests/WorkspaceTests/InternalTests.swift
+++ Tests/WorkspaceTests/InternalTests.swift
@@ -5,8 +5,8 @@
  Diese Quelldatei ist Teil des quelloffenen Arbeitsbereich‐Projekt.
  https://github.com/SDGGiesbrecht/Workspace#workspace
 
- Copyright ©2016–2022 Jeremy David Giesbrecht and the Workspace project contributors.
- Urheberrecht ©2016–2022 Jeremy David Giesbrecht und die Mitwirkenden des Arbeitsbereich‐Projekts.
+ Copyright ©2016–2023 Jeremy David Giesbrecht and the Workspace project contributors.
+ Urheberrecht ©2016–2023 Jeremy David Giesbrecht und die Mitwirkenden des Arbeitsbereich‐Projekts.
 
  Soli Deo gloria.
 

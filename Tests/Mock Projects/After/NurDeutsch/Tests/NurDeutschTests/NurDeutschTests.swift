dd408fcf469368d68ca00f0c01de3022f8d72b78
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-04-10 19:31:13 -0600

    ...

--- Tests/Mock Projects/After/NurDeutsch/Tests/NurDeutschTests/NurDeutschTests.swift
+++ Tests/Mock Projects/After/NurDeutsch/Tests/NurDeutschTests/NurDeutschTests.swift
@@ -2,18 +2,16 @@ import XCTest
 @testable import NurDeutsch
 
 final class NurDeutschTests: XCTestCase {
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(NurDeutsch().text, "Hello, World!")
-  }
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NurDeutsch().text, "Hello, World!")
+    }
 
-  static var allTests = [
-    ("testExample", testExample)
-  ]
+    static var allTests = [
+        ("testExample", testExample),
+    ]
 }
 
-refuse
-to
-build
+refuse to build

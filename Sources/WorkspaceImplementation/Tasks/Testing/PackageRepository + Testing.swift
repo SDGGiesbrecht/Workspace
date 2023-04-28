63392ed1713fdaca8cb0926c0f0e890d6601c5b4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-04-27 21:53:36 -0600

    Suppressed warning.

--- Sources/WorkspaceImplementation/Tasks/Testing/PackageRepository + Testing.swift
+++ Sources/WorkspaceImplementation/Tasks/Testing/PackageRepository + Testing.swift
@@ -72,7 +72,12 @@
                   line.line.contains(
                     "lib_InternalSwiftSyntaxParser.dylib) was built for newer macOS version".scalars
                       .literal()
-                  ))
+                  )
+                    // #workaround(Swift 5.8, Issue with SwiftPM.)
+                    ∨ line.line.contains(
+                      "warning: couldn\u{27}t find pc file for sqlite3"
+                    )
+                  )
               }
               log.lines = LineView<String>(filtered)
 

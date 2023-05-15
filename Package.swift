3f511b01e2a965a81eaba2f4626b44cb5f938b5f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-15 16:47:47 -0600

    Disabled some tests.

--- Package.swift
+++ Package.swift
@@ -953,7 +953,7 @@ for target in package.targets {
   })
 }
 
-// #workaround(Swift 5.7 is permitted on macOS due to hardware constraints; the tools version does not reflect intended support elsewhere.)
+// #workaround(Swift 5.7.2, Hardware compatibility; tools version does not reflect support.))
 #if compiler(<5.8) && !os(macOS)
   #error("Swift 5.7 is only supported on macOS; elsewhere, please use Swift 5.8 or select an older version of Workspace.")
 #endif
